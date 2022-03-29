0e7904b22440bd5b05009190207db6a9e797ac0e
klingebiel <lukas.klingebiel@web.de>
2022-03-29 15:55:53 +0200

    refactor(docs): move docs directory into root

--- docs/storybook/jest.config.js
+++ docs/storybook/jest.config.js
@@ -5,5 +5,5 @@ module.exports = {
 		"^.+\\.[tj]sx?$": "babel-jest"
 	},
 	moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
-	coverageDirectory: "../../../coverage/libs/docs/storybook"
+	coverageDirectory: "../../../coverage/docs/storybook"
 };
