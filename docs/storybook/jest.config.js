48e663ac4b7284d1b65886787671cc07912b0eba
klingebiel <lukas.klingebiel@web.de>
2022-04-06 16:43:35 +0200

    chore(storybook): remove library

--- docs/storybook/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	displayName: "docs-storybook",
-	preset: "../../../jest.preset.js",
-	transform: {
-		"^.+\\.[tj]sx?$": "babel-jest"
-	},
-	moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
-	coverageDirectory: "../../../coverage/docs/storybook"
-};
--- docs/storybook/src/example.tsx
+++
@@ -1,7 +0,0 @@
-import "../.storybook/tailwind-imports.css";
-
-export function Example({ text }: { text: string }) {
-	return <button className="bg-purple-800 px-8 py-1 rounded text-white font-bold">{text}</button>;
-}
-
-export default Example;
