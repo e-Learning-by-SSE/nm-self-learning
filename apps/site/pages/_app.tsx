518f76e3d80c320e6e14357a0dfa337f806e70cf
klingebiel <lukas.klingebiel@web.de>
2022-04-15 17:59:45 +0200

    chore(deps): install react-query

--- apps/site/pages/_app.tsx
+++ apps/site/pages/_app.tsx
@@ -1,16 +1,30 @@
 import { AppProps } from "next/app";
 import Head from "next/head";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 import "./styles.css";
+
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			staleTime: Infinity,
+			retry: false
+		}
+	}
+});
 
 function CustomApp({ Component, pageProps }: AppProps) {
 	return (
 		<>
-			<Head>
-				<title>Self-Learning</title>
-			</Head>
-			<main className="min-h-screen">
-				<Component {...pageProps} />
-			</main>
+			<QueryClientProvider client={queryClient}>
+				<Head>
+					<title>Self-Learning</title>
+				</Head>
+				<main className="min-h-screen">
+					<Component {...pageProps} />
+				</main>
+				<ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
+			</QueryClientProvider>
 		</>
 	);
 }
