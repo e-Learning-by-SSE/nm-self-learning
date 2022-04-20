28a00c7e0ca0850e6351b83944dba69548b3d507
klingebiel <lukas.klingebiel@web.de>
2022-04-20 17:22:16 +0200

    refactor(lesson): reintroduce html5 video player

--- apps/site/pages/lessons/[lessonSlug].tsx
+++ apps/site/pages/lessons/[lessonSlug].tsx
@@ -1,5 +1,5 @@
 import { PlayIcon } from "@heroicons/react/solid";
-import { cmsTypes, getNanomoduleBySlug } from "@self-learning/cms-api";
+import { getNanomoduleBySlug } from "@self-learning/cms-api";
 import { AuthorProps, AuthorsList } from "@self-learning/ui/common";
 import { Playlist } from "@self-learning/ui/lesson";
 import { GetStaticPaths, GetStaticProps } from "next";
@@ -81,10 +81,9 @@ const fakeLessons: Lesson[] = [
 ];
 
 export default function Nanomodule({ nanomodule }: NanomoduleProps) {
-	const { title, subtitle, description, slug } = nanomodule;
-	const imgUrl = `http://localhost:1337${nanomodule.image?.data?.attributes?.url}`;
-	const imgAlt = nanomodule.image?.data?.attributes?.alternativeText ?? "";
-	const { url } = nanomodule.content[0] as cmsTypes.ComponentNanomoduleYoutubeVideo;
+	const { title } = nanomodule;
+	//const { url } = nanomodule.content[0] as cmsTypes.ComponentNanomoduleYoutubeVideo;
+	const url = "http://localhost:1337/uploads/sample_video_7b4e24c005.mp4";
 
 	const authors = nanomodule.authors?.data.map(
 		author =>
@@ -101,7 +100,7 @@ export default function Nanomodule({ nanomodule }: NanomoduleProps) {
 				<title>{title}</title>
 			</Head>
 
-			<div className="flex min-h-screen flex-col bg-neutral-200 md:py-16">
+			<div className="flex min-h-screen flex-col bg-neutral-200 md:pb-16 md:pt-4">
 				<div className="mx-auto flex w-full flex-col gap-4 lg:container">
 					<VideoPlayerWithPlaylist videoUrl={url as string} />
 					<LessonHeader lesson={nanomodule} authors={authors} />
@@ -113,11 +112,12 @@ export default function Nanomodule({ nanomodule }: NanomoduleProps) {
 
 function VideoPlayerWithPlaylist({ videoUrl }: { videoUrl: string }) {
 	return (
-		<div className="grid w-full gap-8 xl:grid-cols-4">
-			<div className="h-[512px] lg:h-[728px] xl:col-span-3">
-				<YoutubeEmbed url={videoUrl}></YoutubeEmbed>
+		<div className="grid w-full xl:grid-cols-4">
+			<div className="h-[512px] lg:h-[720px] xl:col-span-3">
+				{/* <YoutubeEmbed url={videoUrl}></YoutubeEmbed> */}
+				<VideoPlayer url={videoUrl} />
 			</div>
-			<div className="max-h-[728px] overflow-hidden xl:col-span-1">
+			<div className="max-h-[512px] overflow-hidden xl:col-span-1 xl:max-h-[720px]">
 				<Playlist
 					lessons={fakeLessons}
 					currentLesson={fakeLessons[0]}
@@ -136,7 +136,7 @@ function YoutubeEmbed({ url }: { url: string }) {
 		<iframe
 			height="100%"
 			width="100%"
-			src={`https://www.youtube-nocookie.com//embed/${videoId}`}
+			src={`https://www.youtube-nocookie.com/embed/${videoId}`}
 			title="YouTube video player"
 			allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
 			allowFullScreen
@@ -144,32 +144,29 @@ function YoutubeEmbed({ url }: { url: string }) {
 	);
 }
 
-// function VideoPlayer({ url }: { url: string}) {
-// 	<video controls src={url} type={}></video>;
-// }
-
-// export function NanomoduleDynamicZone(content: NanomoduleContentDynamicZone) {
-// 	if (content.__typename === "ComponentNanomoduleYoutubeVideo") {
-// 		return <YoutubeEmbed url={content.url as string} />;
-// 	}
-
-// 	if (content.__typename === "ComponentNanomoduleVideo") {
-// 		return
-// 	}
-// }
+function VideoPlayer({ url }: { url: string }) {
+	return (
+		<div className="flex h-full w-full bg-black">
+			<video controls src={url}></video>
+		</div>
+	);
+}
 
 function LessonHeader({ lesson, authors }: { lesson: Nanomodule; authors: AuthorProps[] }) {
 	return (
-		<div className="flex flex-col-reverse justify-between gap-4 px-2 2xl:flex-row 2xl:px-0">
-			<div className="flex flex-grow flex-col">
+		<div className="flex flex-col">
+			<div className="multi-gradient card flex flex-wrap justify-between gap-8">
 				<AuthorsList authors={authors} />
-				<h1 className="mb-8 mt-8 text-4xl md:text-6xl">{lesson.title}</h1>
+				<button className="btn-primary flex w-full flex-wrap-reverse md:w-fit">
+					<span>Zur Lernkontrolle</span>
+					<PlayIcon className="h-6 shrink-0" />
+				</button>
+			</div>
+
+			<div className="flex flex-grow flex-col">
+				<h1 className="mb-8 mt-8 text-4xl xl:text-6xl">{lesson.title}</h1>
 				{lesson.subtitle && <div className="text-lg tracking-tight">{lesson.subtitle}</div>}
 			</div>
-			<button className="btn-primary h-fit flex-grow-0 xl:w-fit ">
-				<span>Zur Lernkontrolle</span>
-				<PlayIcon className="h-6" />
-			</button>
 		</div>
 	);
 }
