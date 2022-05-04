c645db6a6931b9d48210badcda5e6ab9e484b7cf
klingebiel <lukas.klingebiel@web.de>
2022-05-04 15:26:30 +0200

    feat(image-card): add optional badge slot

--- libs/ui/common/src/lib/image-card/image-card.tsx
+++ libs/ui/common/src/lib/image-card/image-card.tsx
@@ -5,16 +5,18 @@ export function ImageCard({
 	title,
 	imgUrl,
 	subtitle,
-	footer
+	footer,
+	badge
 }: {
 	title: string;
 	subtitle: string | null;
 	slug: string;
 	imgUrl: string | null;
 	footer?: ReactElement;
+	badge?: ReactElement;
 }) {
 	return (
-		<div className="flex flex-col">
+		<div className="relative flex flex-col">
 			{!imgUrl || imgUrl.length === 0 ? (
 				<div className="relative h-[256px] w-full shrink-0 rounded-t-lg bg-gradient-to-br from-purple-500 to-blue-400"></div>
 			) : (
@@ -29,6 +31,8 @@ export function ImageCard({
 				</div>
 			)}
 
+			{badge && <div className="absolute p-2">{badge}</div>}
+
 			<div className="glass flex h-full flex-col justify-between gap-4 rounded-b-lg p-4">
 				<div className="flex flex-col gap-4">
 					<h2 className="text-2xl">{title}</h2>
@@ -37,5 +41,24 @@ export function ImageCard({
 				{footer && <div className="flex flex-col">{footer}</div>}
 			</div>
 		</div>
+	);
+}
+
+export function ImageCardBadge({
+	text,
+	className
+}: {
+	text: string;
+	/** Tailwind background color, i.e. `bg-emerald-500`. */
+	className?: string;
+}) {
+	return (
+		<span
+			className={`rounded-lg py-1 px-4 text-sm font-semibold text-white ${
+				className ? className : ""
+			}`}
+		>
+			{text}
+		</span>
 	);
 }
