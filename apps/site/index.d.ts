dfbd3fe581ad2a80bdaab7a4eedf522d1e136cb2
klingebiel <lukas.klingebiel@web.de>
2022-05-08 12:04:03 +0200

    refactor: add doc comment to ResolvedValue type

--- apps/site/index.d.ts
+++ apps/site/index.d.ts
@@ -5,7 +5,13 @@ declare module "*.svg" {
 	export default content;
 }
 
-/** Test */
+/**
+ * Type alias that excludes `null` and `undefined` from the return type of an async function.
+ *
+ * @example
+ * async function tryGetCourse(): Promise<CourseObject | null | undefined> { ... }
+ * type Course = ResolvedValue<typeof tryGetCourse>; // CourseObject
+ */
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 type ResolvedValue<Fn extends (...args: any) => unknown> = Exclude<
 	Awaited<ReturnType<Fn>>,
