66d1acc9fd7733755ff30a9252e8a862b0fde3f3
klingebiel <lukas.klingebiel@web.de>
2022-05-16 12:44:47 +0200

    chore: move env variables from cms to root

--- apps/cms/config/server.js
+++ apps/cms/config/server.js
@@ -1,7 +1,19 @@
+console.log("ENV:");
+
+console.log(`STRAPI_WEBHOOK_TOKEN: ${process.env.STRAPI_WEBHOOK_TOKEN}`);
+console.log(`STRAPI_HOST: ${process.env.STRAPI_HOST}`);
+console.log(`STRAPI_PORT: ${process.env.STRAPI_PORT}`);
+console.log(`STRAPI_APP_KEYS: ${process.env.STRAPI_APP_KEYS}`);
+
 module.exports = ({ env }) => ({
-	host: env("HOST", "0.0.0.0"),
-	port: env.int("PORT", 1337),
+	host: env("HOST", process.env.STRAPI_HOST),
+	port: env.int("PORT", process.env.STRAPI_PORT),
 	app: {
-		keys: env.array("APP_KEYS")
+		keys: process.env.STRAPI_APP_KEYS.split(",")
+	},
+	webhooks: {
+		defaultHeaders: {
+			Authorization: process.env.STRAPI_WEBHOOK_TOKEN
+		}
 	}
 });
