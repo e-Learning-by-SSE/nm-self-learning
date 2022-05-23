2742b091674c6da50b3486208c190e26932e97c3
klingebiel <lukas.klingebiel@web.de>
2022-05-23 12:45:25 +0200

    refactor(landing-page): improve responsiveness

--- apps/site/pages/index.tsx
+++ apps/site/pages/index.tsx
@@ -3,7 +3,7 @@ import { ReactComponent as StudyingSvg } from "../svg/studying.svg";
 
 export function LandingPage() {
 	return (
-		<div className="relative flex h-full flex-col gap-16">
+		<div className="relative flex h-full flex-col gap-16 px-4">
 			<div className="absolute left-4 top-4 flex flex-col text-sm text-slate-400">
 				<Link href="/subjects">
 					<a className="underline">Fachgebiete</a>
@@ -15,22 +15,26 @@ export function LandingPage() {
 					<a className="underline">Example Course</a>
 				</Link>
 			</div>
-			<div className="mx-auto flex flex-wrap gap-32 px-64">
-				<div className="z-20 flex max-w-2xl shrink-0 flex-col py-48">
-					<h1 className="text-8xl">SELF-le@rning</h1>
-					<h2 className="mt-4 text-3xl font-light text-slate-400">
+
+			<div className="container mx-auto grid items-center py-24 lg:grid-cols-2 lg:py-48">
+				<div className="z-20 flex max-w-2xl shrink-0 flex-col">
+					<h1 className="text-5xl lg:text-8xl">SELF-le@rning</h1>
+					<h2 className="mt-4 text-2xl font-light text-slate-400 lg:text-3xl">
 						Universität Hildesheim
 					</h2>
 
-					<span className="mt-16 text-xl text-slate-600">
+					<span className="mt-16 text-lg text-slate-600 md:text-xl">
 						Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos, nisi. Vitae
 						magnam perferendis officia, fugiat a, odit velit deleniti consequatur
 						dignissimos sit esse itaque soluta ex corrupti accusantium illo numquam.
 					</span>
 
-					<button className="btn-primary mt-16 w-fit">Call to Action</button>
+					<button className="mt-16 w-fit rounded-lg bg-secondary px-12 py-3 text-xl font-semibold text-white">
+						Call to Action
+					</button>
 				</div>
-				<div className="h-[728px] w-[728px] shrink-0 pt-32">
+
+				<div className="pt-16 md:pt-0">
 					<StudyingSvg />
 				</div>
 			</div>
