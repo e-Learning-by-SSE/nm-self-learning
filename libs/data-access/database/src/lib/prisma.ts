c30ec79caabeb1210a30b7a97c56d5846c6901e0
klingebiel <lukas.klingebiel@web.de>
2022-06-01 20:34:11 +0200

    fix(database): prevent "PrismaClient is unable to be run in the browser" error
    See https://github.com/prisma/prisma/issues/6219

--- libs/data-access/database/src/lib/prisma.ts
+++ libs/data-access/database/src/lib/prisma.ts
@@ -1,22 +1,31 @@
-/* eslint-disable @typescript-eslint/ban-ts-comment */
-// /lib/prisma.ts
-import { PrismaClient } from "@prisma/client";
-
 // PrismaClient is attached to the `global` object in development to prevent
 // exhausting your database connection limit.
 // Learn more: https://pris.ly/d/help/next-js-best-practices
+// https://github.com/prisma/prisma/issues/6219
+
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+	namespace NodeJS {
+		interface Global {
+			prisma: PrismaClient;
+		}
+	}
+}
 
 /** Object that provides access to the database. */
-export let database: PrismaClient;
+export let database: PrismaClient = null as unknown as PrismaClient;
 
-if (process.env.NODE_ENV === "production") {
-	database = new PrismaClient();
-} else {
-	// @ts-ignore
-	if (!global.prisma) {
+if (typeof window === "undefined") {
+	if (process.env.NODE_ENV === "production") {
+		database = new PrismaClient();
+	} else {
 		// @ts-ignore
-		global.prisma = new PrismaClient();
+		if (!global.prisma) {
+			// @ts-ignore
+			global.prisma = new PrismaClient();
+		}
+		// @ts-ignore
+		database = global.prisma;
 	}
-	// @ts-ignore
-	database = global.prisma;
 }
