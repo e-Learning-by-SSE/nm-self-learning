e9c90ea90176db3fda79617108263a54bd412f28
klingebiel <lukas.klingebiel@web.de>
2022-06-16 21:12:50 +0200

    fix: use placeholders for supabase env variables
    Supabase is not required in production at the moment.

--- libs/feature/teaching/src/lib/supabase.ts
+++ libs/feature/teaching/src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string ?? "placeholderSupabaseUrl";
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string ?? "placeholderSupabaseKey";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
