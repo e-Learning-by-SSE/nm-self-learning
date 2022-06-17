9d114f41ccf9b718f1ce4a611eac5b6327c0b8f9
klingebiel <lukas.klingebiel@web.de>
2022-06-17 17:07:05 +0200

    fix(markdown): improve consistency with application styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ const { join } = require("path");
 
 const defaultTheme = require("tailwindcss/defaultTheme");
 
+/** @type {import("tailwindcss").Config}  */
 module.exports = {
 	content: [
 		join(__dirname, "./**/!(*.stories|*.spec).tsx"),
@@ -11,6 +12,33 @@ module.exports = {
 	],
 	theme: {
 		extend: {
+			typography: theme => ({
+				DEFAULT: {
+					css: {
+						code: {
+							color: theme("colors.indigo.500")
+						},
+						img: {
+							borderRadius: theme("borderRadius.lg")
+						},
+						h1: {
+							fontWeight: "700",
+							letterSpacing: "-0.05em",
+							color: theme("colors.slate.800")
+						},
+						h2: {
+							fontWeight: "700",
+							letterSpacing: "-0.05em",
+							color: theme("colors.slate.800")
+						},
+						h3: {
+							fontWeight: "700",
+							letterSpacing: "-0.05em",
+							color: theme("colors.slate.800")
+						}
+					}
+				}
+			}),
 			fontFamily: {
 				sans: ["Inter", ...defaultTheme.fontFamily.sans]
 			},
