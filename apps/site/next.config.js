b3b2471ebdd288f9fa946a4c0bd0c5c04eddee26
klingebiel <lukas.klingebiel@web.de>
2022-08-31 16:26:21 +0200

    fix: disable superjson
    Breaks layout components.

--- apps/site/next.config.js
+++ apps/site/next.config.js
@@ -19,16 +19,6 @@ const nextConfig = {
 		// Set this to true if you would like to to use SVGR
 		// See: https://github.com/gregberge/svgr
 		svgr: true
-	},
-	experimental: {
-		swcPlugins: [
-			[
-				"next-superjson-plugin",
-				{
-					excluded: []
-				}
-			]
-		]
 	}
 };
 
