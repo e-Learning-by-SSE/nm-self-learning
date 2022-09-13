8d34fafb9b2dfe780f7ec8ad6dbe2e6290e4871a
Sascha El-Sharkawy <elscha@sse.uni-hildesheim.de>
2022-09-13 08:38:06 +0200

    Seed: More adequate picture for psychologie

--- libs/data-access/database/src/lib/seed.ts
+++ libs/data-access/database/src/lib/seed.ts
@@ -70,9 +70,9 @@ const subjects: Prisma.SubjectCreateManyInput[] = [
 		title: "Psychologie",
 		subtitle: faker.lorem.sentences(2),
 		cardImgUrl:
-			"https://images.unsplash.com/photo-1651687965938-93e47d7683f5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=256&q=80",
+			"https://c.pxhere.com/photos/90/ed/brain_mind_psychology_idea_hearts_love_drawing_split_personality-1370218.jpg!d",
 		imgUrlBanner:
-			"https://images.unsplash.com/photo-1651687965938-93e47d7683f5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80"
+			"https://c.pxhere.com/photos/90/ed/brain_mind_psychology_idea_hearts_love_drawing_split_personality-1370218.jpg!d"
 	}
 ];
 
