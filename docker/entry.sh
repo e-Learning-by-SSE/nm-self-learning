2c7229f59a0aad59953ddfdb7e88dcae615b7d55
Sascha El-Sharkawy <elscha@sse.uni-hildesheim.de>
2022-10-07 09:22:55 +0200

    CI: Creation of Docker image

--- docker/entry.sh
+++ docker/entry.sh
@@ -8,33 +8,36 @@ if [ ! -z "${DB_HOST}" ]; then
     done
 fi
 
-
-# Determine DB connection string (may be passed via paramter to container)
-if [ -z "${DATABASE_URL}" ]; then
-  # Load from file if not passed as parameter
-  source /app/.env
-fi
-
-if [ ! -e /app/initialization ]; then
-    echo "First boot, initialize app with DB connection: ${DATABASE_URL}"
+# Check if any configuration parameters have been changed
+# Add grep '^prefix_' to filter for relevant variables
+# Store all OS environment variables (includes variables passed by Docker)
+env -0 | sort -z | tr '\0' '\n' > outside.env
+sed -i -e 's/=/="/;s/$/"/' outside.env
+# Automatically export defined variables, based on: https://unix.stackexchange.com/a/79077
+set -a
+# Load variables of container (as default, will overwirte OS variables)
+#sed -e 's/=/="/;s/$/"/' .env > old.env
+#source old.env
+#rm -f old.env
+source .env
+# Load tmp.env to restore passed variables
+source outside.env
+set +a
+# Store variables (sorted to make comparison more stable)
+# Add grep '^prefix_' to filter for relevant variables
+env -0 | sort -z | tr '\0' '\n' > tmp.env
+# Check if list of environment varaibles differ (or even doesn't exist)
+if [ ! -z .env ] || ! cmp -s tmp.env .env; then
+    echo "Environment was changed: Recompile app"
+    mv .env .env_bak
+    cp -f tmp.env .env
+    sed -i -e 's/=/="/;s/$/"/' .env
     
     # Next.js requires rebuild whenever DB location was changed
     npm run build
     
     # Clear database and apply sample data on first boot
     npm run prisma migrate reset --force --skip-generate
-
-    echo "${DATABASE_URL}" > /app/initialization
-elif [ $(< /app/initialization) != "${DATABASE_URL}" ]; then
-    echo "Reconfigure app with DB connection: ${DATABASE_URL}"
-    
-    # Next.js requires rebuild whenever DB location was changed
-    npm run build
-    
-    # Clear database and apply sample data on first boot
-    npm run prisma migrate reset --force --skip-generate
-
-    echo "${DATABASE_URL}" > /app/initialization
 fi
 
 # Start Next.js
