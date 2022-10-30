62423f89bc3d16c27bbd1fe688f847a028b401fd
klingebiel <lukas.klingebiel@web.de>
2022-10-30 16:59:21 +0100

    feat(profile): show actual course progress

--- apps/site/pages/profile.tsx
+++ apps/site/pages/profile.tsx
@@ -1,13 +1,12 @@
+/* eslint-disable @next/next/no-img-element */
 import { database } from "@self-learning/database";
-import { ImageCard } from "@self-learning/ui/common";
+import { ImageCard, ImageCardBadge } from "@self-learning/ui/common";
 import { CenteredSection, ItemCardGrid } from "@self-learning/ui/layouts";
 import { formatDistance } from "date-fns";
 import { de } from "date-fns/locale";
 import { GetServerSideProps, GetServerSidePropsResult } from "next";
 import { getToken } from "next-auth/jwt";
-import Image from "next/image";
 import Link from "next/link";
-import { Fragment } from "react";
 
 type ProfileProps = {
 	user: ResolvedValue<typeof getUser>;
@@ -24,17 +23,20 @@ function getUser(username: string) {
 			username: true,
 			displayName: true,
 			enrollments: {
+				orderBy: { progress: "asc" },
 				select: {
 					status: true,
 					createdAt: true,
 					completedAt: true,
+					progress: true,
 					course: {
 						select: {
 							courseId: true,
 							title: true,
 							slug: true,
 							subtitle: true,
-							imgUrl: true
+							imgUrl: true,
+							meta: true
 						}
 					}
 				}
@@ -109,39 +111,17 @@ export const getServerSideProps: GetServerSideProps<ProfileProps> = async ({ req
 
 export default function Profile({ user, completedLessons }: ProfileProps) {
 	return (
-		<>
-			<CenteredSection className="bg-gray-50">
-				<div className="flex flex-col gap-16 lg:flex-row-reverse">
-					<div className="flex flex-col place-items-center lg:place-items-start">
-						<div className="relative h-[256px] w-[256px] rounded-lg bg-black">
-							<Image
-								src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=256&q=80"
-								height={256}
-								width={256}
-								alt=""
-								className="rounded-lg object-cover object-top"
-							></Image>
-						</div>
-						<h1 className="mt-8 text-4xl">{user.displayName}</h1>
-
-						<div className="mt-8 grid gap-1">
-							<span className="text-light">Studiengang:</span>
-							<span className="font-semibold text-secondary">
-								Wirtschaftsinformatik, B. Sc.
-							</span>
-						</div>
-					</div>
-
-					<div className="flex grow flex-col rounded-lg bg-white p-8">
-						<span className="mb-4 text-lg font-semibold ">Aktivität</span>
-						<Activity completedLessons={completedLessons.data} />
-					</div>
+		<div className="flex flex-col bg-gray-50">
+			<CenteredSection>
+				<div className="flex flex-col-reverse gap-16 md:flex-row">
+					<Activity completedLessons={completedLessons.data} />
+					<StudentInfo user={user} />
 				</div>
 			</CenteredSection>
 
-			<CenteredSection className="bg-white">
+			<CenteredSection>
 				<div className="flex flex-col gap-12">
-					<h2 className="text-3xl">Aktuelle Kurse</h2>
+					<h2 className="text-3xl">Meine Kurse</h2>
 
 					<ItemCardGrid>
 						{user.enrollments.map(enrollment => (
@@ -153,23 +133,19 @@ export default function Profile({ user, completedLessons }: ProfileProps) {
 									<ImageCard
 										slug={enrollment.course.slug}
 										title={enrollment.course.title}
-										subtitle={
-											"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat sapiente laudantium inventore quia sed."
-										}
+										subtitle={enrollment.course.subtitle}
 										imgUrl={enrollment.course.imgUrl}
-										footer={
-											<div className="mt-4 flex flex-col">
-												<span className="mx-auto font-semibold">
-													50% bearbeitet
-												</span>
-												<div className="mt-2 h-3 rounded-lg bg-gray-200">
-													<div
-														className="h-3 rounded-lg bg-green-600"
-														style={{ width: "50%" }}
-													></div>
-												</div>
-											</div>
+										badge={
+											enrollment.status === "COMPLETED" ? (
+												<ImageCardBadge
+													className="bg-emerald-500 text-white"
+													text="Abgeschlossen"
+												/>
+											) : (
+												<></>
+											)
 										}
+										footer={<ProgressFooter progress={enrollment.progress} />}
 									/>
 								</a>
 							</Link>
@@ -177,40 +153,7 @@ export default function Profile({ user, completedLessons }: ProfileProps) {
 					</ItemCardGrid>
 				</div>
 			</CenteredSection>
-
-			<CenteredSection className="bg-gray-50">
-				<div className="flex flex-col gap-12">
-					<h2 className="text-3xl">Abgeschlossene Kurse</h2>
-					<ItemCardGrid>
-						{user.enrollments.map(enrollment => (
-							<div key={enrollment.course.courseId}>
-								<ImageCard
-									slug={enrollment.course.slug}
-									title={enrollment.course.title}
-									subtitle={
-										"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat sapiente laudantium inventore quia sed."
-									}
-									imgUrl={enrollment.course.imgUrl}
-									footer={
-										<div className="mt-4 flex flex-col">
-											<span className="mx-auto font-semibold">
-												100% bearbeitet
-											</span>
-											<div className="mt-2 h-3 rounded-lg bg-gray-200">
-												<div
-													className="h-3 rounded-lg bg-green-600"
-													style={{ width: "100%" }}
-												></div>
-											</div>
-										</div>
-									}
-								/>
-							</div>
-						))}
-					</ItemCardGrid>
-				</div>
-			</CenteredSection>
-		</>
+		</div>
 	);
 }
 
@@ -221,43 +164,65 @@ function toDateAgo(date: Date | string | number) {
 	});
 }
 
+function StudentInfo({ user }: { user: ProfileProps["user"] }) {
+	return (
+		<div className="flex flex-col place-items-center lg:place-items-start">
+			<div className="relative h-[256px] w-[256px] rounded-lg bg-black">
+				<img
+					src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=256&q=80"
+					height={256}
+					width={256}
+					alt=""
+					className="h-[256px] w-[256px] rounded-lg object-cover object-top"
+				></img>
+			</div>
+			<h1 className="mt-8 text-4xl">{user.displayName}</h1>
+
+			<div className="mt-8 grid gap-1">
+				<span className="text-light">Studiengang:</span>
+				<span className="font-semibold text-secondary">Wirtschaftsinformatik, B. Sc.</span>
+			</div>
+		</div>
+	);
+}
+
 function Activity({
 	completedLessons
 }: {
 	completedLessons: ProfileProps["completedLessons"]["data"];
 }) {
 	return (
-		<>
+		<section className="flex w-full flex-col gap-4">
+			<h2 className="mb-4 text-3xl">Meine Aktivität</h2>
+
 			{completedLessons.length > 0 ? (
-				<ul className="grid items-start">
+				<ul className="flex flex-col gap-2">
 					{completedLessons.map(lesson => (
-						<Fragment key={lesson.createdAt as unknown as string}>
-							<li className="flex grow flex-wrap items-center justify-between gap-2 rounded-lg">
-								<div className="flex flex-col gap-1">
-									<Link
-										href={`/courses/${lesson.course?.slug}/lessons/${lesson.lesson.slug}`}
-									>
-										<a className="text-sm hover:text-secondary">
-											{lesson.lesson.title}
-										</a>
-									</Link>
-									{lesson.course && (
-										<span className="pl-4 text-sm text-secondary ">
-											in{" "}
-											<Link href={`/courses/${lesson.course.slug}`}>
-												<a className="hover:text-secondary">
-													{lesson.course.title}
-												</a>
-											</Link>
-										</span>
-									)}
-								</div>
-								<span className="self-start text-sm text-light">
-									{toDateAgo(lesson.createdAt)}
-								</span>
-							</li>
-							<span className="my-4 h-[1px] bg-light-border last:invisible last:my-0"></span>
-						</Fragment>
+						<li
+							key={lesson.createdAt as unknown as string}
+							className="flex grow flex-wrap items-center justify-between gap-2 rounded-lg border border-light-border bg-white py-2 px-4"
+						>
+							<div className="flex flex-col gap-1">
+								<Link
+									href={`/courses/${lesson.course?.slug}/${lesson.lesson.slug}`}
+								>
+									<a className="text-sm font-medium hover:text-secondary">
+										{lesson.lesson.title}
+									</a>
+								</Link>
+								{lesson.course && (
+									<span className="text-xs text-light">
+										in{" "}
+										<Link href={`/courses/${lesson.course.slug}`}>
+											<a className="text-secondary">{lesson.course.title}</a>
+										</Link>
+									</span>
+								)}
+							</div>
+							<span className="text-sm text-light">
+								{toDateAgo(lesson.createdAt)}
+							</span>
+						</li>
 					))}
 				</ul>
 			) : (
@@ -265,6 +230,24 @@ function Activity({
 					Du hast bisher noch keine Lerneinheit abgeschlossen.
 				</span>
 			)}
-		</>
+		</section>
+	);
+}
+
+function ProgressFooter({ progress }: { progress: number }) {
+	return (
+		<span className="relative h-5 w-full rounded-lg bg-gray-200">
+			<span
+				className="absolute left-0 h-5 rounded-lg bg-secondary"
+				style={{ width: `${progress}%` }}
+			></span>
+			<span
+				className={`absolute top-0 w-full px-2 text-start text-sm font-semibold ${
+					progress === 0 ? "text-secondary" : "text-white"
+				}`}
+			>
+				{progress}%
+			</span>
+		</span>
 	);
 }
