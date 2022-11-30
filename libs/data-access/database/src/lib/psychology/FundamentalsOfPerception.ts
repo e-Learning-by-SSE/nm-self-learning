d36e26d22a019f86a74f568123d07623e8fcf735
Sascha El-Sharkawy <elscha@sse.uni-hildesheim.de>
2022-11-30 15:41:21 +0100

    Seed: Psychology example extended

--- libs/data-access/database/src/lib/psychology/FundamentalsOfPerception.ts
+++ libs/data-access/database/src/lib/psychology/FundamentalsOfPerception.ts
@@ -6,45 +6,165 @@ import {
 	createTextQuestion
 } from "../seed-functions";
 
-export const chapters = [
-	{
-		title: "Kapitel 1 zur Wahrnemung",
-		description: "Eine Beschreibung",
-		content: [
-			createLesson(
-				"Titel eines Nanomoduls",
-				"Überschrift 2. Ebene oder null",
-				"Beschreibung oder null",
-				[createVideo("link zum Video", 354)],
-				[
-					createMultipleChoice(
-						"Frage",
-						[
-							{
-								content: "Antwortmöglichkeit 1",
-								isCorrect: true
-							},
-							{
-								content: "Antwortmöglichkeit 2",
-								isCorrect: true
-							},
-							{
-								content: "Antwortmöglichkeit n",
-								isCorrect: false
-							}
-						],
-						["Optional: Hinweis 1", "Hinweis 2", "Hinweis n"]
-					),
-					createTextQuestion(
-						"Eine Freitextfrage",
-						["Akzeptierte Antwort 1", "Akzeptierte Antwort 2", "Akzeptierte Antwort n"],
-						["Optional: Hinweis 1", "Optional: Hinweis 2", "Optional: Hinweis n"]
-					)
-				]
-			)
-		]
-	}
-];
+const ch_Signaluebertragung = {
+	title: "1. Signalübertragung im Nervensystem",
+	description:
+		"Überlegen Sie jeweils vorher, was Sie schon zu den Themen wissen, bevor Sie sich die Videos anschauen.",
+	content: [
+		createLesson(
+			"1. Struktur und Funktion des Nervensystems",
+			"Aktivierungsfrage: In welche zwei unterschiedlichen Systeme wird das periphere Nervensystem aufgeteilt?",
+			"Becker-Carus & Wendt, 2017: Allgemeine Psychologie, Seite 33 - 34",
+			[createVideo("https://www.youtube.com/watch?v=18JPgHnUvLQ", 354)],
+			[
+				createMultipleChoice(
+					"In welche zwei unterschiedlichen Systeme wird das periphere Nervensystem aufgeteilt?",
+					[
+						{
+							content: "Willkürliches und unwillkürliches (vegatives) Nervensystem",
+							isCorrect: true
+						},
+						{
+							content: "Gehirn und Rückenmark",
+							isCorrect: false
+						},
+						{
+							content: "somatisches und vegetatives Nervensystem",
+							isCorrect: true
+						},
+						{
+							content:
+								"sensorische (afferente) und motorische (efferente) Untereinheiten",
+							isCorrect: true
+						}
+					]
+				),
+				createTextQuestion(
+					"In welche zwei unterschiedlichen Systeme wird das periphere Nervensystem aufgeteilt?",
+					[
+						"willkürliches und unwillkürliches (vegatives) Nervensystem",
+						"somatisches und vegetatives Nervensystem",
+						"sensorische (afferente) und motorische (efferente) Untereinheiten"
+					]
+				)
+			]
+		),
+		createLesson(
+			"2. Die Nervenzelle",
+			"Aktivierungsfrage: Welche Klassen von Neuronen gibt es?",
+			"Becker-Carus & Wendt, 2017: Allgemeine Psychologie, Seite 34 - 35",
+			[createVideo("https://youtu.be/prK8Hw0GCDg", 354)],
+			[
+				createMultipleChoice("Welche Klassen von Neuronen gibt es?", [
+					{
+						content: "Sensorische, motorische und zwischengeschaltete Neuronen",
+						isCorrect: true
+					},
+					{
+						content: "marklose oder unmyelinisierte Nervenfasern",
+						isCorrect: false
+					},
+					{
+						content:
+							"Neurone, die die Impulse, die sie von den Rezeptoren der verschiedenen Sinnesorgane empfangen, zum ZNS übertragen.",
+						isCorrect: true
+					},
+					{
+						content:
+							"Neurone, die die Signale, die vom Gehirn oder Rückemark kommen, zu den Effektorganen, den Muskeln und Drüsen weiterleiten.",
+						isCorrect: true
+					}
+				]),
+				createTextQuestion("Welche Klassen von Neuronen gibt es", [
+					"Sensorische, motorische und zwischengeschaltete Neuronen",
+					"Neurone, die die Impulse, die sie von den Rezeptoren der verschiedenen Sinnesorgane empfangen, zum ZNS übertragen.",
+					"Neurone, die die Signale, die vom Gehirn oder Rückemark kommen, zu den Effektorganen, den Muskeln und Drüsen weiterleiten."
+				])
+			]
+		)
+	]
+};
+
+const ch_Organisation = {
+	title: "2. Organisation im Nervensystem",
+	description:
+		"Überlegen Sie jeweils vorher, was Sie schon zu den Themen wissen, bevor Sie sich die Videos anschauen.",
+	content: [
+		createLesson(
+			"10. Zentralnervensystem",
+			"Aktivierungsfrage: In welche zwei unterschiedlichen Systeme wird das periphere Nervensystem aufgeteilt?",
+			"Becker-Carus & Wendt, 2017: Allgemeine Psychologie, Seite 42 - 43",
+			[createVideo("https://www.youtube.com/watch?v=18JPgHnUvLQ", 354)],
+			[
+				createMultipleChoice(
+					"In welche zwei unterschiedlichen Systeme wird das periphere Nervensystem aufgeteilt?",
+					[
+						{
+							content: "Willkürliches und unwillkürliches (vegatives) Nervensystem",
+							isCorrect: true
+						},
+						{
+							content: "Gehirn und Rückenmark",
+							isCorrect: false
+						},
+						{
+							content: "somatisches und vegetatives Nervensystem",
+							isCorrect: true
+						},
+						{
+							content:
+								"sensorische (afferente) und motorische (efferente) Untereinheiten",
+							isCorrect: true
+						}
+					]
+				),
+				createTextQuestion(
+					"In welche zwei unterschiedlichen Systeme wird das periphere Nervensystem aufgeteilt?",
+					[
+						"willkürliches und unwillkürliches (vegatives) Nervensystem",
+						"somatisches und vegetatives Nervensystem",
+						"sensorische (afferente) und motorische (efferente) Untereinheiten"
+					]
+				)
+			]
+		),
+		createLesson(
+			"11. Peripheres Nervensystem",
+			"Aktivierungsfrage: Welche Klassen von Neuronen gibt es?",
+			"Becker-Carus & Wendt, 2017: Allgemeine Psychologie, Seite 43 - 44",
+			[createVideo("https://youtu.be/prK8Hw0GCDg", 354)],
+			[
+				createMultipleChoice("Welche Klassen von Neuronen gibt es?", [
+					{
+						content: "Sensorische, motorische und zwischengeschaltete Neuronen",
+						isCorrect: true
+					},
+					{
+						content: "marklose oder unmyelinisierte Nervenfasern",
+						isCorrect: false
+					},
+					{
+						content:
+							"Neurone, die die Impulse, die sie von den Rezeptoren der verschiedenen Sinnesorgane empfangen, zum ZNS übertragen.",
+						isCorrect: true
+					},
+					{
+						content:
+							"Neurone, die die Signale, die vom Gehirn oder Rückemark kommen, zu den Effektorganen, den Muskeln und Drüsen weiterleiten.",
+						isCorrect: true
+					}
+				]),
+				createTextQuestion("Welche Klassen von Neuronen gibt es", [
+					"Sensorische, motorische und zwischengeschaltete Neuronen",
+					"Neurone, die die Impulse, die sie von den Rezeptoren der verschiedenen Sinnesorgane empfangen, zum ZNS übertragen.",
+					"Neurone, die die Signale, die vom Gehirn oder Rückemark kommen, zu den Effektorganen, den Muskeln und Drüsen weiterleiten."
+				])
+			]
+		)
+	]
+};
+
+export const chapters = [ch_Signaluebertragung, ch_Organisation];
 
 export const course = createCourse(
 	"psychologie",
