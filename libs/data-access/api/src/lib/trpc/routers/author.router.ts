ab6967b1a925ca33774483f5adf1e2d095814534
klingebiel <lukas.klingebiel@web.de>
2022-11-30 18:42:27 +0100

    feat: author and permission management (wip)

--- libs/data-access/api/src/lib/trpc/routers/author.router.ts
+++ libs/data-access/api/src/lib/trpc/routers/author.router.ts
@@ -1,6 +1,6 @@
 import { database } from "@self-learning/database";
 import { z } from "zod";
-import { authProcedure, t } from "../trpc";
+import { adminProcedure, authProcedure, t } from "../trpc";
 
 export const authorRouter = t.router({
 	getBySlug: authProcedure.input(z.object({ slug: z.string() })).query(({ input }) => {
@@ -21,5 +21,74 @@ export const authorRouter = t.router({
 				imgUrl: true
 			}
 		});
-	})
+	}),
+	getAllWithSubject: adminProcedure.query(() => {
+		return database.user.findMany({
+			where: { NOT: { author: null } },
+			orderBy: { author: { displayName: "asc" } },
+			select: {
+				name: true,
+				role: true,
+				author: {
+					select: {
+						slug: true,
+						displayName: true,
+						imgUrl: true,
+						subjectAdmin: {
+							select: {
+								subject: {
+									select: {
+										title: true
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		});
+	}),
+	getAuthorForForm: adminProcedure
+		.input(
+			z.object({
+				username: z.string()
+			})
+		)
+		.query(({ input }) => {
+			return database.user.findUniqueOrThrow({
+				where: { name: input.username },
+				select: {
+					name: true,
+					role: true,
+					author: {
+						select: {
+							slug: true,
+							displayName: true,
+							imgUrl: true,
+							username: true,
+							subjectAdmin: {
+								select: {
+									subject: {
+										select: {
+											title: true,
+											subjectId: true
+										}
+									}
+								}
+							},
+							specializationAdmin: {
+								select: {
+									specialization: {
+										select: {
+											specializationId: true,
+											title: true
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			});
+		})
 });
