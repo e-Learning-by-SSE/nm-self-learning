a4a1174d252256c1d2f96ceabfc0ce0b14639671
klingebiel <lukas.klingebiel@web.de>
2022-12-08 12:59:53 +0100

    feat(storage): register uploaded assets

--- libs/data-access/api/src/lib/trpc/routers/storage.router.ts
+++ libs/data-access/api/src/lib/trpc/routers/storage.router.ts
@@ -1,7 +1,9 @@
+import { Prisma } from "@prisma/client";
+import { database } from "@self-learning/database";
 import { getRandomId } from "@self-learning/util/common";
 import { Client, ClientOptions } from "minio";
 import { z } from "zod";
-import { authProcedure, t } from "../trpc";
+import { adminProcedure, authProcedure, t } from "../trpc";
 
 export const minioConfig: ClientOptions & { bucketName: string; publicUrl?: string } = z
 	.object({
@@ -49,7 +51,7 @@ export const storageRouter = t.router({
 				publicUrl: `${publicUrlWithBucket}/${randomizedFilename}`
 			};
 		}),
-	removeFile: authProcedure
+	removeFile: adminProcedure
 		.input(
 			z.object({
 				filename: z.string()
@@ -57,7 +59,36 @@ export const storageRouter = t.router({
 		)
 		.mutation(({ input }) => {
 			return removeFile(input.filename);
-		})
+		}),
+	registerAsset: authProcedure
+		.input(
+			z.object({
+				filename: z.string(),
+				publicUrl: z.string(),
+				filetype: z.string()
+			})
+		)
+		.mutation(({ ctx, input }) => {
+			const data: Prisma.UploadedAssetsCreateManyInput = {
+				username: ctx.user.name,
+				filename: input.filename,
+				publicUrl: input.publicUrl,
+				filetype: input.filetype,
+				createdAt: new Date().toISOString()
+			};
+
+			return database.uploadedAssets.upsert({
+				where: { publicUrl: input.publicUrl },
+				update: data,
+				create: data
+			});
+		}),
+	getMyAssets: authProcedure.query(({ ctx }) => {
+		return database.uploadedAssets.findMany({
+			where: { username: ctx.user.name },
+			orderBy: { createdAt: "desc" }
+		});
+	})
 });
 
 /** Uses the `minio` SDK to request a presigned URL that users can upload files to. */
