6bab83da03551546c4f20180137efe102cbadb49
spark-sse <spark@uni-hildesheim.de>
2023-02-27 15:37:19 +0100

    fix(login): fix wrong path when NEXT_PUBLIC_BASE_PATH is undefined
    bug was introduced in the latest commit d4254a4312d37ddf756ef74b4fbbefdf8f7382a91~

--- libs/ui/layouts/src/lib/redirect-to-login.ts
+++ libs/ui/layouts/src/lib/redirect-to-login.ts
@@ -1,5 +1,7 @@
 import { signIn, signOut } from "next-auth/react";
 
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
+
 /**
  * Redirects the user to the login page.
  *
@@ -7,7 +9,7 @@ import { signIn, signOut } from "next-auth/react";
  * Otherwise, they will redirected directly to the Keycloak login page.
  */
 export function redirectToLogin(): void {
-	const callbackUrl = `${window.location.origin}/${process.env.NEXT_PUBLIC_BASE_PATH}`;
+	const callbackUrl = `${window.location.origin}/${basePath}`;
 	if (process.env.NEXT_PUBLIC_IS_DEMO_INSTANCE === "true") {
 		signIn(undefined, { callbackUrl: callbackUrl });
 	} else {
@@ -19,6 +21,6 @@ export function redirectToLogin(): void {
  * Redirect the user to the main app after logout.
  */
 export function redirectToLogout() : void {
-	const callbackUrl = `${window.location.origin}/${process.env.NEXT_PUBLIC_BASE_PATH}`;
+	const callbackUrl = `${window.location.origin}/${basePath}`;
 	signOut({callbackUrl: callbackUrl });
 }
