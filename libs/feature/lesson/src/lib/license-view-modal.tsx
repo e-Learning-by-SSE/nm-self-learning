6ed983548e8b1bf1a36fe5184ec2459b6a8c63c0
Lukas Dönges <lukasdoenges@t-online.de>
2023-04-29 10:06:09 +0200

    feat(oer) Support for Markdown in licenseText

--- libs/feature/lesson/src/lib/license-view-modal.tsx
+++ libs/feature/lesson/src/lib/license-view-modal.tsx
@@ -2,6 +2,7 @@ import { Dialog} from "@self-learning/ui/common";
 import { LabeledField} from "@self-learning/ui/forms";
 import { CenteredContainer } from "@self-learning/ui/layouts";
 import { useState } from "react";
+import { MarkdownViewer } from "@self-learning/ui/forms";
 
 export function LicenseViewModal({ description, onClose }: { description: string, onClose: () => void }) {
 	const [open, setOpen] = useState(true);
@@ -13,7 +14,7 @@ export function LicenseViewModal({ description, onClose }: { description: string
 				style={{ height: "30vh", width: "60vw", overflow: "auto" }}
 				title={"License Description"} onClose={() => {setOpen(false); onClose()}}>
 				<CenteredContainer>
-					<LabeledField label={description} />
+					<MarkdownViewer content={description} />
 				</CenteredContainer>
 				</Dialog>
 			</CenteredContainer>
