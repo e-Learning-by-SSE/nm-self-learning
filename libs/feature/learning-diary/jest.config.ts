8a7dfab4e080ce720ae0ac8def5dfe2054fc36f7
fabian.kneer <fabian.kneer@fh-dortmund.de>
2023-10-23 13:15:25 +0200

    fix(ldiary): fix jest config for feature-learning-diary

--- libs/feature/learning-diary/jest.config.ts
+++ libs/feature/learning-diary/jest.config.ts
@@ -1,9 +1,15 @@
 /* eslint-disable */
 export default {
-	displayName: "feature-learning-diary",
+	displayName: "feature-lesson",
 	preset: "../../../jest.preset.js",
+	globals: {},
 	transform: {
-		"^.+\\.[tj]sx?$": ["@swc/jest", { jsc: { transform: { react: { runtime: "automatic" } } } }]
+		"^.+\\.[tj]sx?$": [
+			"ts-jest",
+			{
+				tsconfig: "<rootDir>/tsconfig.spec.json"
+			}
+		]
 	},
 	moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
 	coverageDirectory: "../../../coverage/libs/feature/learning-diary"
