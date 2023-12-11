e9d13147791f08ac8792a26fb44159c1e06e25f3
Lukas Doenges <doenges@uni-hildesheim.de>
2023-12-11 12:30:32 +0000

    fix(folder-editor) fixes detection hook for multi select

--- libs/feature/teaching/src/lib/skills/folder-editor/cycle-detection/detection-hook.tsx
+++ libs/feature/teaching/src/lib/skills/folder-editor/cycle-detection/detection-hook.tsx
@@ -8,30 +8,34 @@ interface DrawState {
 	initial: boolean
 }
 
-const listener: Map<string, (state: DrawState) => void> = new Map();
+const listener: Map<string, Array<(state: DrawState) => void>> = new Map();
 const memorizedDetection: FolderItem[] = [];
 
 export const dispatchDetection = (items: FolderItem[]) => {
 	items.forEach(item => {
-		const listenerCallback = listener.get(item.skill.id);
-		if (listenerCallback) {
+		const listenerCallbacks = listener.get(item.skill.id);
+		if (listenerCallbacks) {
 			if(item.cycle || item.parent) {
 				memorizedDetection.push(item);
 			}
-			listenerCallback({item: item, initial: false});
+			for(const callback of listenerCallbacks) {
+				callback({item: item, initial: false});
+			}
 		}
 	});
 };
 
 export const ClearCycleDetection = () => {
 	memorizedDetection.forEach(item => {
-		const listenerCallback = listener.get(item.skill.id);
-		if (listenerCallback) {
+		const listenerCallbacks = listener.get(item.skill.id);
+		if (listenerCallbacks) {
 			const folderItem = {
 				skill: item.skill,
 				selectedSkill: item.selectedSkill,
 			};
-			listenerCallback({item: folderItem, initial: false});
+			for(const callback of listenerCallbacks) {
+				callback({item: folderItem, initial: false});
+			}
 		}
 	});
 	memorizedDetection.length = 0;
@@ -42,7 +46,23 @@ export function useDetection(id: string): DrawState {
 	const [state, setState] = useState<DrawState>({item: null, initial: true});
 
 	useEffect(() => {
-		listener.set(id, setState);
+		if(!listener.has(id)) {
+			listener.set(id, [setState]);
+			return;
+		} else {
+			const listenerCallbacks = listener.get(id);
+			if(listenerCallbacks) {
+				listenerCallbacks.push(setState);
+			}
+			return () => {
+				if(listenerCallbacks) {
+					const index = listenerCallbacks.indexOf(setState);
+					if(index > -1) {
+						listenerCallbacks.splice(index, 1);
+					}
+				}
+			}
+		}
 	}, [id, state]);
 	return state;
 }
