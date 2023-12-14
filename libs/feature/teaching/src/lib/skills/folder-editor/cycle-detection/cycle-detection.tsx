5b2d18e3eeb992a89998ba5c7b6e6967066535f4
Marc Palfner <palfner-sse@gmx.de>
2023-12-14 12:18:36 +0100

    fix(folder-editor): Rename 'ClearCycleDetection' to 'clearCycleDetection' and reformat code as per ESLint standards

--- libs/feature/teaching/src/lib/skills/folder-editor/cycle-detection/cycle-detection.tsx
+++ libs/feature/teaching/src/lib/skills/folder-editor/cycle-detection/cycle-detection.tsx
@@ -1,7 +1,7 @@
-import {SkillFormModel} from "@self-learning/types";
-import {findParentsOfCycledSkills} from "@self-learning/skills-pathfinder";
-import {ClearCycleDetection, dispatchDetection} from "./detection-hook";
-import {Alert, dispatchDialog, freeDialog, SimpleDialog} from "@self-learning/ui/common";
+import { SkillFormModel } from "@self-learning/types";
+import { findParentsOfCycledSkills } from "@self-learning/skills-pathfinder";
+import { clearCycleDetection, dispatchDetection } from "./detection-hook";
+import { Alert, dispatchDialog, freeDialog, SimpleDialog } from "@self-learning/ui/common";
 
 /**
  *
@@ -16,29 +16,27 @@ export async function checkForCycles(skillMap: Map<string, FolderItem>, item?: F
 			id: skill.id,
 			name: skill.name,
 			repositoryId: skill.repositoryId,
-			nestedSkills: skill.children,
+			nestedSkills: skill.children
 		};
 	});
 
-	const parents = findParentsOfCycledSkills(skills)
+	const parents = findParentsOfCycledSkills(skills);
 
-	ClearCycleDetection();
+	clearCycleDetection();
 
-	if(!parents)  {
+	if (!parents) {
 		//No cycle found
 
 		freeDialog("alert");
-		if(item) {
-			dispatchDetection([item]); 
+		if (item) {
+			dispatchDetection([item]);
 		}
-		
 	} else {
 		//cycle found
 
-
 		const cycles = parents.cycles;
 		const parentList = parents.nestingSkills;
-	
+
 		const folderItems: FolderItem[] = [];
 		for (const cycle of cycles) {
 			for (const cycleItem of cycle) {
@@ -47,63 +45,81 @@ export async function checkForCycles(skillMap: Map<string, FolderItem>, item?: F
 					folderItem = item;
 				}
 				if (!folderItem) continue;
-				const newFolderItem = {...folderItem, cycle: "taskbar"};
+				const newFolderItem = { ...folderItem, cycle: "taskbar" };
 				skillMap.set(cycleItem.id, newFolderItem);
 				folderItems.push(newFolderItem);
 			}
 		}
-	
+
 		for (const parent of parentList) {
 			const folderItem = skillMap.get(parent.id);
 			if (!folderItem) continue;
-			const newFolderItem = {...folderItem, parent: "taskbar"};
+			const newFolderItem = { ...folderItem, parent: "taskbar" };
 			skillMap.set(parent.id, newFolderItem);
 			folderItems.push(newFolderItem);
 		}
-	
-		const cycleStackTrace = getCycleStackTrace(cycles as { name: string }[][])
+
+		const cycleStackTrace = getCycleStackTrace(cycles as { name: string }[][]);
 		//draws the cycle symbol only in the affected rows.
 		dispatchDetection(folderItems);
 		if (!(cycles.length > 0)) return;
 		const onClickDetailedCycle = () => {
-			dispatchDialog(<SimpleDialog description={cycleStackTrace} name={"Zyklen - Darstellung"}
-										 onClose={() => freeDialog("simpleDialog")}/>, "simpleDialog")
-		}
+			dispatchDialog(
+				<SimpleDialog
+					description={cycleStackTrace}
+					name={"Zyklen - Darstellung"}
+					onClose={() => freeDialog("simpleDialog")}
+				/>,
+				"simpleDialog"
+			);
+		};
 		//draws the error message
-		dispatchDialog(<Alert type={{
-			severity: "ERROR", message:
-	
-				<div>
-					<button onClick={onClickDetailedCycle}
-							className="hover:text-red-700 hover:cursor-pointer text-left"><span>
-					Warnung: In Ihrer Modelierung der Lernziele liegt ein Zyclus vor.
-					Aufgrunddessen kann der Algorithmus keinen Gültigen Lernfpad mit denen im Zyclus beteiligten
-					Lernzielen finden.
-					Angegebene Pfadziel sind nicht erreichbar und nur Privat speicherbar.
-					Die betroffenden Skills können
-					durch Klicken auf diesen Text angezeigt werden.	</span></button>
-				</div>
-	
-		}}/>, "alert")
+		dispatchDialog(
+			<Alert
+				type={{
+					severity: "ERROR",
+					message: (
+						<div>
+							<button
+								onClick={onClickDetailedCycle}
+								className="text-left hover:cursor-pointer hover:text-red-700"
+							>
+								<span>
+									Warnung: In Ihrer Modelierung der Lernziele liegt ein Zyclus
+									vor. Aufgrunddessen kann der Algorithmus keinen Gültigen
+									Lernfpad mit denen im Zyclus beteiligten Lernzielen finden.
+									Angegebene Pfadziel sind nicht erreichbar und nur Privat
+									speicherbar. Die betroffenden Skills können durch Klicken auf
+									diesen Text angezeigt werden.{" "}
+								</span>
+							</button>
+						</div>
+					)
+				}}
+			/>,
+			"alert"
+		);
 	}
-
 }
 
 export function getCycleStackTrace<S extends { name: string }>(cycles: S[][]): string {
-	let result = ""
+	let result = "";
 	cycles.forEach(cycle => {
 		cycle.forEach((item, index) => {
 			if (index === cycle.length - 1) {
 				result += item.name + " -> ";
-				result += cycle[0].name
+				result += cycle[0].name;
 				return;
 			}
 			result += item.name + " -> ";
-
 		});
 	});
 	return result;
 }
 
-
-export type FolderItem = { skill: SkillFormModel, selectedSkill: boolean, cycle?: string, parent?: string };
+export type FolderItem = {
+	skill: SkillFormModel;
+	selectedSkill: boolean;
+	cycle?: string;
+	parent?: string;
+};
--- libs/feature/teaching/src/lib/skills/folder-editor/cycle-detection/detection-hook.tsx
+++ libs/feature/teaching/src/lib/skills/folder-editor/cycle-detection/detection-hook.tsx
@@ -1,11 +1,9 @@
-import {FolderItem} from "./cycle-detection";
-import {useEffect, useState} from "react";
-
-
+import { FolderItem } from "./cycle-detection";
+import { useEffect, useState } from "react";
 
 interface DrawState {
-	item: FolderItem | null,
-	initial: boolean
+	item: FolderItem | null;
+	initial: boolean;
 }
 
 const listener: Map<string, Array<(state: DrawState) => void>> = new Map();
@@ -15,53 +13,52 @@ export const dispatchDetection = (items: FolderItem[]) => {
 	items.forEach(item => {
 		const listenerCallbacks = listener.get(item.skill.id);
 		if (listenerCallbacks) {
-			if(item.cycle || item.parent) {
+			if (item.cycle || item.parent) {
 				memorizedDetection.push(item);
 			}
-			for(const callback of listenerCallbacks) {
-				callback({item: item, initial: false});
+			for (const callback of listenerCallbacks) {
+				callback({ item: item, initial: false });
 			}
 		}
 	});
 };
 
-export const ClearCycleDetection = () => {
+export const clearCycleDetection = () => {
 	memorizedDetection.forEach(item => {
 		const listenerCallbacks = listener.get(item.skill.id);
 		if (listenerCallbacks) {
 			const folderItem = {
 				skill: item.skill,
-				selectedSkill: item.selectedSkill,
+				selectedSkill: item.selectedSkill
 			};
-			for(const callback of listenerCallbacks) {
-				callback({item: folderItem, initial: false});
+			for (const callback of listenerCallbacks) {
+				callback({ item: folderItem, initial: false });
 			}
 		}
 	});
 	memorizedDetection.length = 0;
-}
-
+};
 
 export function useDetection(id: string): DrawState {
-	const [state, setState] = useState<DrawState>({item: null, initial: true});
+	const [state, setState] = useState<DrawState>({ item: null, initial: true });
 
 	useEffect(() => {
-		if(!listener.has(id)) {
+		if (!listener.has(id)) {
 			listener.set(id, [setState]);
 			return;
 		} else {
 			const listenerCallbacks = listener.get(id);
-			if(listenerCallbacks) {
+			if (listenerCallbacks) {
 				listenerCallbacks.push(setState);
 			}
 			return () => {
-				if(listenerCallbacks) {
+				if (listenerCallbacks) {
 					const index = listenerCallbacks.indexOf(setState);
-					if(index > -1) {
+					if (index > -1) {
 						listenerCallbacks.splice(index, 1);
 					}
 				}
-			}
+			};
 		}
 	}, [id, state]);
 	return state;
