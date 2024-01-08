4d542574a120d60b7b3327e5bdc4b84a4124cf05
Arne Perschke <arne.perschke@web.de>
2024-01-08 10:04:37 +0100

    refactor(search): allowed search result amount to be set by attribute

--- libs/ui/layouts/src/lib/search-section.tsx
+++ libs/ui/layouts/src/lib/search-section.tsx
@@ -4,18 +4,22 @@ export function SearchSection({
 	title,
 	results,
 	baseLink,
-	setSearchQuery
+	setSearchQuery,
+	maxDisplayedSearchResults = 4, // how many search results are at most displayed for the section
+	searchResultOffset = 0 // Where in the results the display should start
 }: {
 	title: string;
 	results: { title: string; slug: string }[];
 	baseLink: string;
 	setSearchQuery: (changes: string) => void;
+	maxDisplayedSearchResults?: number;
+	searchResultOffset?: number;
 }) {
 	return (
 		<>
 			<div className="bg-gray-200 p-2">{title}</div>
 			<div className="w-full overflow-hidden text-ellipsis">
-				{results.slice(0, 4).map(result => (
+				{results.slice(searchResultOffset, maxDisplayedSearchResults).map(result => (
 					<Link
 						className="block w-full overflow-hidden text-ellipsis p-2 hover:bg-emerald-500 hover:text-white"
 						key={result.title}
