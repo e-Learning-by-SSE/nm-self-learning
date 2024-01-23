489d290fe671d47c436235bfb8f684680b85f306
Sascha El-Sharkawy <elscha@sse.uni-hildesheim.de>
2024-01-23 13:25:49 +0100

    rf(Progressbar): Renamed parameter
    hideOnComplete -> hideOnFinish

--- libs/ui/common/src/lib/progress-bar/progress-bar.tsx
+++ libs/ui/common/src/lib/progress-bar/progress-bar.tsx
@@ -1,17 +1,17 @@
 /**
  * A reusable progress bar component, which uses the platform's color scheme (emerald).
  * @param progress A function which indicates the current progress in percent (0 = not started; 100 = finished)
- * @param hideOnComplete If true, the progress bar will be hidden after the progress has finished
+ * @param hideOnFinish If true, the progress bar will be hidden after the progress has finished
  * @returns A React component to visualize the progress
  */
 export function ProgressBar({
 	progress,
-	hideOnComplete = true
+	hideOnComplete: hideOnFinish = true
 }: {
 	progress: number;
 	hideOnComplete?: boolean;
 }) {
-	if (progress < 100 || !hideOnComplete) {
+	if (progress < 100 || !hideOnFinish) {
 		return (
 			<div className="mb-4 h-2.5 w-full rounded-full bg-gray-200 dark:bg-gray-700">
 				<div
