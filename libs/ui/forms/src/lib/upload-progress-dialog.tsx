5ae5aa0d4ad1626274461d7a2077cc0115c8072c
Sascha El-Sharkawy <elscha@sse.uni-hildesheim.de>
2024-01-24 08:57:15 +0100

    rf(Upload): Progress Bar controlled by caller; UploadDialog JSDoc

--- libs/ui/forms/src/lib/upload-progress-dialog.tsx
+++ libs/ui/forms/src/lib/upload-progress-dialog.tsx
@@ -2,6 +2,14 @@ import { DialogWithReactNodeTitle, ProgressBar } from "@self-learning/ui/common"
 import { CenteredContainer } from "@self-learning/ui/layouts";
 import { useState, useEffect } from "react";
 
+/**
+ * Dialog that indicates the progress of a file upload.
+ * @param name The name of the file that is currently uploaded
+ * @param progress The progress in percent of the file upload
+ * (0 = not started (opens the dialog); 100 = finished (closes the dialog))
+ * @param onClose A function that is called when the dialog is closed (e.g. external setState function to close the dialog)
+ * @returns A dialog visualizing the progress of the file upload
+ */
 export function UploadProgressDialog({
 	name,
 	progress,
