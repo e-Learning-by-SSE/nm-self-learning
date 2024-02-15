c582bc8c050838c4f5902616d33ca55b87b15db7
lukas-png <65257500+lukas-png@users.noreply.github.com>
2024-02-15 21:35:58 +0100

    fix(markdown): client side exception when typing language name in markdown code blocks (#116)
    ---------
    
    Co-authored-by: spark-sse <113825305+spark-sse@users.noreply.github.com>

--- libs/util/common/src/lib/strings.ts
+++ libs/util/common/src/lib/strings.ts
@@ -1,3 +1,5 @@
 export function stringOrNull(str: string | null | undefined) {
 	return str && str.length > 0 ? str : null;
 }
+
+export const isString = (value: unknown): value is string => typeof value === "string";
