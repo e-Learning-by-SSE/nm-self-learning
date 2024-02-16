8f4c6253780b6e98006cb8a3bbf037aa771262ed
spark-sse <113825305+spark-sse@users.noreply.github.com>
2024-02-16 15:49:54 +0000

    rf(search-bar): seperate components to reduce complexity
    Currently a good loading infrastructure is missing. When the user starts typing, the not found message is shown.

--- libs/ui/layouts/src/lib/search-bar.tsx
+++ libs/ui/layouts/src/lib/search-bar.tsx
@@ -5,30 +5,105 @@ import { trpc } from "@self-learning/api-client";
 import { useSession } from "next-auth/react";
 import { SearchSection } from "./search-section";
 
-export function SearchBar() {
+function SearchInput({
+	searchQuery,
+	setSearchQuery
+}: {
+	searchQuery: string;
+	setSearchQuery: (value: string) => void;
+}) {
+	return (
+		<div className="relative">
+			<div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+				<SearchIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+			</div>
+			<input
+				autoComplete="off"
+				id="search"
+				name="search"
+				className="block w-full rounded-md border-0 bg-white py-1.5 pl-10 pr-3 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-emerald-500 lg:text-sm lg:leading-6"
+				placeholder="Suchen..."
+				type="search"
+				value={searchQuery}
+				onChange={e => {
+					setSearchQuery(e.target.value);
+				}}
+			/>
+		</div>
+	);
+}
+
+function NoResults() {
+	return (
+		<div className="px-4 py-8 text-center text-sm sm:px-8">
+			<EmojiSadIcon className="mx-auto h-7 w-7 text-gray-400" />
+			<p className="mt-4 font-semibold text-gray-900">Nichts hier</p>
+			<p className="mt-2 text-gray-500">Leider konnten wir nichts finden was</p>
+			<p className="text-gray-500">deiner Suchanfrage entspricht</p>
+		</div>
+	);
+}
+
+function SearchResults({
+	searchQuery,
+	resetSearchQuery
+}: {
+	searchQuery: string;
+	resetSearchQuery: () => void;
+}) {
 	const session = useSession();
 	const user = session.data?.user;
+	const search = { title: searchQuery, page: 1 };
+	const isUserAdmin = user?.role === "ADMIN";
 
+	const { data: lessons, isLoading: lessonLoading } = trpc.lesson.findMany.useQuery(search);
+
+	const { data: authors, isLoading: authorLoading } = trpc.author.findMany.useQuery(search);
+	const authorResults = authors?.result.map(({ displayName: title, slug }) => ({ title, slug }));
+
+	const { data: courses, isLoading: coursesLoading } = trpc.course.findMany.useQuery(search);
+
+	const hasResults =
+		(lessons && lessons?.result?.length > 0) ||
+		(authors && authors?.result?.length > 0) ||
+		(courses && courses?.result?.length > 0);
+
+	// TODO make use of loading circles here when they are implemented
+	return (
+		<>
+			{!hasResults && <NoResults />}
+			{courses && (
+				<SearchSection
+					title="Kurse"
+					results={courses.result}
+					baseLink="courses"
+					onClick={resetSearchQuery}
+				/>
+			)}
+			{lessons && isUserAdmin && (
+				<SearchSection
+					title="Lerneinheiten"
+					results={lessons.result}
+					baseLink="teaching/lessons/edit"
+					onClick={resetSearchQuery}
+				/>
+			)}
+
+			{authorResults && (
+				<SearchSection
+					title="Autoren"
+					results={authorResults}
+					baseLink="authors"
+					onClick={resetSearchQuery}
+				/>
+			)}
+		</>
+	);
+}
+
+export function SearchBar() {
 	const [searchQuery, setSearchQuery] = useState("");
-
-	const { data: courses } = trpc.course.findMany.useQuery({
-		title: searchQuery,
-		page: 1
-	});
-
-	const { data: lessons } =
-		user?.role === "ADMIN"
-			? trpc.lesson.findMany.useQuery({ title: searchQuery, page: 1 })
-			: { data: null };
-
-	const { data: authors } = trpc.author.findMany.useQuery({
-		displayName: searchQuery,
-		page: 1
-	});
-
-	const resetCallback = () => {
-		setSearchQuery("");
-	};
+	const resetCallback = () => setSearchQuery("");
 
 	return (
 		<div className="hidden flex-1 items-center justify-center px-2 lg:ml-6 lg:flex lg:justify-end">
@@ -36,23 +111,7 @@ export function SearchBar() {
 				<label htmlFor="search" className="sr-only">
 					Suche
 				</label>
-				<div className="relative">
-					<div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-						<SearchIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
-					</div>
-					<input
-						autoComplete="off"
-						id="search"
-						name="search"
-						className="block w-full rounded-md border-0 bg-white py-1.5 pl-10 pr-3 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-emerald-500 lg:text-sm lg:leading-6"
-						placeholder="Suchen..."
-						type="search"
-						value={searchQuery}
-						onChange={e => {
-							setSearchQuery(e.target.value);
-						}}
-					/>
-				</div>
+				<SearchInput searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
 
 				<Transition
 					show={true}
@@ -63,74 +122,12 @@ export function SearchBar() {
 					leaveFrom="transform opacity-100 scale-100"
 					leaveTo="transform opacity-0 scale-95"
 				>
-					{searchQuery === "" ? (
-						<div></div>
-					) : (
+					{searchQuery !== "" && (
 						<div className="absolute right-0 z-10 mt-2 w-full origin-top-right truncate rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-							{courses?.result?.length === 0 &&
-							lessons?.result?.length === 0 &&
-							authors?.result?.length === 0 ? (
-								<div className="px-4 py-8 text-center text-sm sm:px-8">
-									<EmojiSadIcon className="mx-auto h-7 w-7 text-gray-400" />
-									<p className="mt-4 font-semibold text-gray-900">Nichts hier</p>
-									<p className="mt-2 text-gray-500">
-										Leider konnten wir nichts finden was
-									</p>
-									<p className="text-gray-500">deiner Suchanfrage entspricht</p>
-								</div>
-							) : (
-								<>
-									{courses && courses.result?.length > 0 ? (
-										<SearchSection
-											title="Kurse"
-											results={courses.result.map(element => {
-												return {
-													title: element.title,
-													slug: element.slug
-												};
-											})}
-											baseLink="courses"
-											onClick={resetCallback}
-										/>
-									) : (
-										<div></div>
-									)}
-
-									{user?.role === "ADMIN" &&
-									lessons &&
-									lessons.result?.length > 0 ? (
-										<SearchSection
-											title="Lerneinheiten"
-											results={lessons.result.map(element => {
-												return {
-													title: element.title,
-													slug: element.lessonId
-												};
-											})}
-											baseLink="teaching/lessons/edit"
-											onClick={resetCallback}
-										/>
-									) : (
-										<div></div>
-									)}
-
-									{authors && authors.result?.length > 0 ? (
-										<SearchSection
-											title="Autoren"
-											results={authors.result.map(element => {
-												return {
-													title: element.displayName,
-													slug: element.slug
-												};
-											})}
-											baseLink="authors"
-											onClick={resetCallback}
-										/>
-									) : (
-										<div></div>
-									)}
-								</>
-							)}
+							<SearchResults
+								searchQuery={searchQuery}
+								resetSearchQuery={resetCallback}
+							/>
 						</div>
 					)}
 				</Transition>
--- libs/ui/layouts/src/lib/search-section.tsx
+++ libs/ui/layouts/src/lib/search-section.tsx
@@ -1,5 +1,7 @@
 import Link from "next/link";
 
+export type SearchResultInfo = { title: string; slug: string };
+
 export function SearchSection({
 	title,
 	results,
@@ -9,16 +11,20 @@ export function SearchSection({
 	searchResultOffset = 0 // Where in the results the display should start
 }: {
 	title: string;
-	results: { title: string; slug: string }[];
+	results: SearchResultInfo[];
 	baseLink: string;
 	onClick: () => void;
 	maxDisplayedSearchResults?: number;
 	searchResultOffset?: number;
 }) {
+	const showEmptySpace = results.length === 0;
 	return (
 		<>
 			<div className="bg-gray-200 p-2">{title}</div>
 			<div className="w-full overflow-hidden text-ellipsis">
+				{showEmptySpace && (
+					<div className="block w-full p-2 italic text-gray-500">Keine Ergebnisse</div>
+				)}
 				{results.slice(searchResultOffset, maxDisplayedSearchResults).map(result => (
 					<Link
 						className="block w-full overflow-hidden text-ellipsis p-2 hover:bg-emerald-500 hover:text-white"
