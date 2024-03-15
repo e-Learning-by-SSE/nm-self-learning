a7881395f15e7349e96f9cae865ab4dc58553647
Sascha El-Sharkawy <elscha@sse.uni-hildesheim.de>
2024-03-15 07:31:17 +0100

    fix(LiaExport): JSZip imported as ECMAScript compatible module import

--- libs/util/lia-exporter/src/lib/lia-exporter.ts
+++ libs/util/lia-exporter/src/lib/lia-exporter.ts
@@ -19,7 +19,7 @@ import {
 	MissedElement
 } from "./types";
 import { convertQuizzes } from "./question-utils";
-import JSZip = require("jszip");
+import * as JSZip from "JSZip";
 import { downloadWithProgress, getFileSize } from "./network-utils";
 /**
  * Generates a zip file (markdown file + media files)that can be imported into LiaScript.
