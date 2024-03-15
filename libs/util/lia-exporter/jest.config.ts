617e7321982c7118a6de609011de70d6f34d3846
Sascha El-Sharkawy <elscha@sse.uni-hildesheim.de>
2024-03-15 08:50:22 +0100

    test(LiaExport): Fixes broken tests
    - Added transformIgnorePatterns to handle export issue of LiaScript library
    - Proper naming for test cases
    - Moved all tests into 1 describe block to form 1 executable test suite for all quiz-related tests
    - Formatted expected results to handle indentation correctly
    - Fixed some wrong input parameters

--- libs/util/lia-exporter/jest.config.ts
+++ libs/util/lia-exporter/jest.config.ts
@@ -7,5 +7,10 @@ export default {
 		"^.+\\.[tj]s$": ["ts-jest", { tsconfig: "<rootDir>/tsconfig.spec.json" }]
 	},
 	moduleFileExtensions: ["ts", "js", "html"],
-	coverageDirectory: "../../../coverage/libs/util/lia-exporter"
+	coverageDirectory: "../../../coverage/libs/util/lia-exporter",
+	/*
+	 * Avoids SyntaxError: Unexpected token export which is produced by LiaScript Library
+	 * See: https://stackoverflow.com/a/49676319
+	 */
+	transformIgnorePatterns: ["node_modules/(?!@ngrx|(?!deck.gl)|ng-dynamic)"]
 };
