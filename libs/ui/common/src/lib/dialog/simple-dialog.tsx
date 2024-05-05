dfa315df134df5ea32d5a36c6f4a71d5c77a9cfb
lukas-png <doenges@uni-hildesheim.de>
2024-05-05 18:20:42 +0000

    feat(skill): better custom for simple dialog

--- libs/ui/common/src/lib/dialog/simple-dialog.tsx
+++ libs/ui/common/src/lib/dialog/simple-dialog.tsx
@@ -1,4 +1,3 @@
-import { MarkdownViewer } from "@self-learning/ui/forms";
 import { CenteredContainer } from "@self-learning/ui/layouts";
 import React, { useState } from "react";
 import { Dialog, DialogActions } from "@self-learning/ui/common";
@@ -13,10 +12,12 @@ export const ButtonActions = {
 export function SimpleDialog({
 	children,
 	name,
+	size = "sm",
 	onClose
 }: {
 	children: React.ReactNode;
 	name: string;
+	size?: "sm" | "md" | "lg";
 	onClose: (action: ButtonActions) => void;
 }) {
 	const [open, setOpen] = useState(true);
@@ -26,18 +27,37 @@ export function SimpleDialog({
 		onClose(type ?? ButtonActions.CANCEL);
 	};
 
+	const getDialogSize = () => {
+		switch (size) {
+			case "sm":
+				return { height: "25vh", width: "30vw" };
+			case "md":
+				return { height: "50vh", width: "50vw" };
+			case "lg":
+				return { height: "75vh", width: "70vw" };
+			default:
+				return { height: "25vh", width: "30vw" };
+		}
+	}
+
+
 	if (!open) return null;
 	return (
 		<CenteredContainer>
 			<Dialog
-				style={{ height: "25vh", width: "30vw", overflow: "auto" }}
+				style={getDialogSize()}
 				title={name}
 				onClose={() => {
 					onDialogClose();
 				}}
 			>
-				<CenteredContainer>{children}</CenteredContainer>
-				<div className="mt-auto">
+				<CenteredContainer className="overflow-auto">
+					<div className="overflow-auto">{children}</div>
+				</CenteredContainer>
+				<div
+					className="dialog-actions-container"
+					style={{ position: "absolute", bottom: 10, right: 10}}
+				>
 					<DialogActions onClose={onDialogClose}>
 						<button
 							className="btn-primary"
