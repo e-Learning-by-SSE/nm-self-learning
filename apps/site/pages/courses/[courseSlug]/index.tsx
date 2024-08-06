f565e7ca64122790e1420801c85e5604513630fd
Marc Palfner <148242116+palfner-sse@users.noreply.github.com>
2024-08-06 13:04:04 +0200

    fix(courseHeader): Fix 404 restart error when lesson was completed (#144)
    Co-authored-by: spark-sse <113825305+spark-sse@users.noreply.github.com>

--- apps/site/pages/courses/[courseSlug]/index.tsx
+++ apps/site/pages/courses/[courseSlug]/index.tsx
@@ -215,6 +215,8 @@ function CourseHeader({
 		return null;
 	}, [completion, content]);
 
+	const firstLessonFromChapter = content[0].content[0];
+	const lessonCompletionCount = completion?.courseCompletion.completedLessonCount ?? 0;
 	return (
 		<section className="flex flex-col gap-16">
 			<div className="flex flex-wrap-reverse gap-12 md:flex-nowrap">
@@ -272,13 +274,17 @@ function CourseHeader({
 
 					{isEnrolled && (
 						<Link
-							href={`/courses/${course.slug}/${nextLessonSlug}`}
+							href={`/courses/${course.slug}/${
+								nextLessonSlug ?? firstLessonFromChapter.slug
+							}`}
 							className="btn-primary"
 						>
 							<span>
-								{completion?.courseCompletion.completedLessonCount === 0
-									? "Starten"
-									: "Fortfahren"}
+								{nextLessonSlug
+									? lessonCompletionCount === 0
+										? "Starten"
+										: "Fortfahren"
+									: "Öffnen"}
 							</span>
 							<PlayIcon className="h-5" />
 						</Link>
