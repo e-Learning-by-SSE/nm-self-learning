cf0bf5da1a969645a2df18cb3f6800fa986af113
klausmp <palfner-sse@gmx.de>
2024-09-09 11:32:39 +0200

    feat(learningDiaryOverview): makes Table rows linked to the Corresponding Entry

--- apps/site/pages/ltb/index.tsx
+++ apps/site/pages/ltb/index.tsx
@@ -12,6 +12,7 @@ import { getSession } from "next-auth/react";
 import { ChevronDoubleDownIcon } from "@heroicons/react/24/outline";
 import { UniversalSearchBar } from "@self-learning/ui/layouts";
 import { getLearningDiaryEntriesOverview, LearningDiaryEntriesOverview } from "@self-learning/api";
+import { useRouter } from "next/router";
 
 //TODO - der link vom kurs + link vom eintrag
 
@@ -58,6 +59,8 @@ function SortedTable({
 		key: "number",
 		descendingOrder: true
 	});
+
+	const router = useRouter();
 
 	const [chevronMenu, setChevronMenu] = useState<boolean>(false);
 
@@ -218,87 +221,90 @@ function SortedTable({
 			<div className="overflow-y-auto rounded-lg border-x border-b border-light-border bg-white">
 				<table className="w-full table-auto">
 					<thead className="top-0 z-10 rounded-lg border-y border-light-border bg-gray-100">
-						<tr>
-							{[...columns.values()]
-								.filter(column => column.isDisplayed)
-								.map(column => {
-									const columnKey = findKeyByValue(columns, column);
+					<tr>
+						{[...columns.values()]
+							.filter(column => column.isDisplayed)
+							.map(column => {
+								const columnKey = findKeyByValue(columns, column);
 
-									return (
-										<th
-											className="cursor-pointer border-y border-light-border py-4 px-8 text-start text-sm font-semibold"
-											key={columnKey}
-											onClick={() =>
-												setSortedColumn({
-													key: columnKey,
-													descendingOrder:
-														sortedColumn.key === columnKey
-															? !sortedColumn.descendingOrder
-															: true
-												})
-											}
-										>
-											<div className="">
-												<span>{column.label}</span>
-												{sortedColumn && sortedColumn.key === columnKey ? (
-													<span className="">
+								return (
+									<th
+										className="cursor-pointer border-y border-light-border py-4 px-8 text-start text-sm font-semibold"
+										key={columnKey}
+										onClick={() =>
+											setSortedColumn({
+												key: columnKey,
+												descendingOrder:
+													sortedColumn.key === columnKey
+														? !sortedColumn.descendingOrder
+														: true
+											})
+										}
+									>
+										<div className="">
+											<span>{column.label}</span>
+											{sortedColumn && sortedColumn.key === columnKey ? (
+												<span className="">
 														{sortedColumn.descendingOrder ? " ▼" : " ▲"}
 													</span>
-												) : (
-													<span className="invisible"> ▲</span>
-												)}
-											</div>
-										</th>
-									);
-								})}
+											) : (
+												<span className="invisible"> ▲</span>
+											)}
+										</div>
+									</th>
+								);
+							})}
 
-							<th
-								className="cursor-pointer border-y border-light-border py-4 px-8 text-start text-sm font-semibold"
-								key="chevron"
-								onClick={() => {
-									setChevronMenu(true);
-								}}
-								onMouseLeave={() => {
-									setChevronMenu(false);
-								}}
-							>
-								<div className="flex justify-between">
-									<ChevronDoubleDownIcon className="h-5 w-5 stroke-current stroke-2 text-black" />
-								</div>
-								{chevronMenu && (
-									<TableDropdownMenu
-										columns={columns}
-										setColumns={setColumns}
-										setChevronMenu={setChevronMenu}
-									/>
-								)}
-							</th>
-						</tr>
+						<th
+							className="cursor-pointer border-y border-light-border py-4 px-8 text-start text-sm font-semibold"
+							key="chevron"
+							onClick={() => {
+								setChevronMenu(true);
+							}}
+							onMouseLeave={() => {
+								setChevronMenu(false);
+							}}
+						>
+							<div className="flex justify-between">
+								<ChevronDoubleDownIcon className="h-5 w-5 stroke-current stroke-2 text-black"/>
+							</div>
+							{chevronMenu && (
+								<TableDropdownMenu
+									columns={columns}
+									setColumns={setColumns}
+									setChevronMenu={setChevronMenu}
+								/>
+							)}
+						</th>
+					</tr>
 					</thead>
 					<tbody className="divide-y divide-light-border">
-						{learningDiaryEntries
-							.slice()
-							.filter(entry => getFilterFunction(entry, searchQuarry))
-							.sort(getSortingFunction())
-							.map(learningDiaryEntry => (
-								<tr key={learningDiaryEntry.id}>
-									{[...columns.values()]
-										.filter(column => column.isDisplayed)
-										.map(column => {
-											const columnKey = findKeyByValue(columns, column);
+					{learningDiaryEntries
+						.slice()
+						.filter(entry => getFilterFunction(entry, searchQuarry))
+						.sort(getSortingFunction())
+						.map(learningDiaryEntry => (
+							<tr key={learningDiaryEntry.id} onClick={() => {
+								router.push("/ltb/entry/" + learningDiaryEntry.number)
+							}} className={"cursor-pointer hover:bg-gray-100"}>
+								{[...columns.values()]
+									.filter(column => column.isDisplayed)
+									.map(column => {
+										const columnKey = findKeyByValue(columns, column);
 
-											return (
-												<TableDataColumn key={column.label}>
-													{columnKey === "number" &&
-														learningDiaryEntry.number}
+										return (
+											<TableDataColumn key={column.label}>
+												{columnKey === "number" &&
+													learningDiaryEntry.number}
 
-													{columnKey === "course" && (
-														<Link
-															href={`/courses/${learningDiaryEntry.course.slug}/`}
-															className="block"
-														>
-															<div>
-																<span className="flex items-center justify-center text-gray-800 hover:text-secondary">
+												{columnKey === "course" && (
+													<Link
+														href={`/courses/${learningDiaryEntry.course.slug}/`}
+														className="block"
+													>
+														<div>
+																<span
+																	className="flex items-center justify-center text-gray-800 hover:text-secondary">
 																	<span className="truncate">
 																		{
 																			learningDiaryEntry
@@ -306,50 +312,50 @@ function SortedTable({
 																		}
 																	</span>
 																</span>
-															</div>
-														</Link>
+														</div>
+													</Link>
+												)}
+
+												{columnKey === "date" &&
+													learningDiaryEntry.date}
+
+												{columnKey === "duration" &&
+													formatTimeIntervalToString(
+														learningDiaryEntry.duration
 													)}
 
-													{columnKey === "date" &&
-														learningDiaryEntry.date}
+												{columnKey === "learningLocation" &&
+													learningDiaryEntry.learningLocation?.name}
 
-													{columnKey === "duration" &&
-														formatTimeIntervalToString(
-															learningDiaryEntry.duration
-														)}
+												{columnKey === "learningStrategie" &&
+													learningDiaryEntry.learningTechniqueEvaluation
+														.map(
+															evaluation =>
+																evaluation.learningTechnique
+																	.learningStrategie.name
+														)
+														.filter(
+															(value, index, self) =>
+																self.indexOf(value) === index
+														)
+														.join(", ")}
 
-													{columnKey === "learningLocation" &&
-														learningDiaryEntry.learningLocation?.name}
+												{columnKey === "learningTechnique" &&
+													learningDiaryEntry.learningTechniqueEvaluation
+														.map(
+															evaluation =>
+																evaluation.learningTechnique
+																	.name
+														)
+														.join(", ")}
 
-													{columnKey === "learningStrategie" &&
-														learningDiaryEntry.learningTechniqueEvaluation
-															.map(
-																evaluation =>
-																	evaluation.learningTechnique
-																		.learningStrategie.name
-															)
-															.filter(
-																(value, index, self) =>
-																	self.indexOf(value) === index
-															)
-															.join(", ")}
-
-													{columnKey === "learningTechnique" &&
-														learningDiaryEntry.learningTechniqueEvaluation
-															.map(
-																evaluation =>
-																	evaluation.learningTechnique
-																		.name
-															)
-															.join(", ")}
-
-													{columnKey === "scope" &&
-														learningDiaryEntry.scope}
-												</TableDataColumn>
-											);
-										})}
-								</tr>
-							))}
+												{columnKey === "scope" &&
+													learningDiaryEntry.scope}
+											</TableDataColumn>
+										);
+									})}
+							</tr>
+						))}
 					</tbody>
 				</table>
 			</div>
