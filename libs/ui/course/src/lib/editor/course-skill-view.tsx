242a18f7cc41e1b079578940a9c71881f1d6ed5c
staciwamonika <staciwam@uni-hildesheim.de>
2024-09-10 12:30:01 +0200

    some changes

--- libs/ui/course/src/lib/editor/course-skill-view.tsx
+++ libs/ui/course/src/lib/editor/course-skill-view.tsx
@@ -1,3 +1,15 @@
+import { CourseFormModel } from "@self-learning/teaching";
+import { useFormContext } from "react-hook-form";
+
 export function CourseSkillView() {
+	const form = useFormContext<CourseFormModel & { content: unknown[] }>();
+	const {
+		register,
+		control,
+		formState: { errors },
+		getValues
+	} = form;
+
+	console.log("Form", getValues());
 	return <>course skill view</>;
 }
