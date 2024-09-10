45d2aadaf08765f17ecb3a5443642ba4f2aaa113
staciwamonika <staciwam@uni-hildesheim.de>
2024-09-10 12:30:01 +0200

    some changes

--- libs/ui/course/src/lib/editor/course-basic-information.tsx
+++ libs/ui/course/src/lib/editor/course-basic-information.tsx
@@ -1,13 +1,32 @@
 import { ExtendedCourseFormValues } from "@self-learning/teaching";
-import { useFormContext } from "react-hook-form";
-import { useEffect, useState } from "react";
-import { InputWithButton, LabeledField, useSlugify } from "@self-learning/ui/forms";
+import { Controller, useFormContext } from "react-hook-form";
+import { memo, useEffect, useState } from "react";
+import {
+	InputWithButton,
+	LabeledField,
+	SearchField,
+	Upload,
+	useSlugify
+} from "@self-learning/ui/forms";
 import { AuthorsForm } from "libs/feature/teaching/src/lib/author/authors-form";
 import { CourseSkillForm } from "libs/feature/teaching/src/lib/lesson/forms/skills-form";
 import { SidebarSectionTitle } from "libs/ui/forms/src/lib/form-container";
-import { IconButton } from "@self-learning/ui/common";
+import {
+	Dialog,
+	DialogActions,
+	IconButton,
+	ImageOrPlaceholder,
+	LoadingBox,
+	OnDialogCloseFn
+} from "@self-learning/ui/common";
 import { PlusIcon } from "@heroicons/react/24/solid";
 import { trpc } from "@self-learning/api-client";
+import { Author } from "@self-learning/types";
+import {
+	InlineRemoveButton,
+	SelectSkillsView
+} from "libs/feature/teaching/src/lib/skills/skill-dialog/select-skill-view";
+import { SelectSkillDialog } from "libs/feature/teaching/src/lib/skills/skill-dialog/select-skill-dialog";
 
 export function CourseBasicInformation() {
 	const form = useFormContext<ExtendedCourseFormValues>();
@@ -17,6 +36,16 @@ export function CourseBasicInformation() {
 		control,
 		formState: { errors }
 	} = form;
+
+	const { data: authors = [] } = trpc.author.getAll.useQuery();
+
+	const onSelectorChange = (authorSlug: string) => {
+		console.log("on selector change", authorSlug);
+	};
+
+	const onAddAuthor = () => {};
+	const onDeleteAuthor = () => {};
+
 	return (
 		<div className="m-2 grid w-2/3 grid-cols-1 gap-6 p-2 md:grid-cols-2">
 			<div>
@@ -25,12 +54,213 @@ export function CourseBasicInformation() {
 
 			<div>
 				<Skills />
-				<Selectors />
+				<Selectors
+					authors={authors}
+					onAddAuthor={onAddAuthor}
+					onDeleteAuthor={onDeleteAuthor}
+				/>
 			</div>
 		</div>
 	);
 }
 
+type AuthorSelector = {
+	displayName: string;
+	slug: string; // TODO: is slug unique?
+};
+
+function Selectors({
+	authors,
+	onAddAuthor,
+	onDeleteAuthor
+}: {
+	authors: AuthorSelector[];
+	onAddAuthor: (authorSlug: AuthorSelector[] | undefined) => void;
+	onDeleteAuthor: (author: AuthorSelector) => void;
+}) {
+	const [selectAuthorSelector, setSelectAuthorSelectorModal] = useState(false);
+
+	return (
+		<>
+			<SidebarSectionTitle title="Selektoren" subtitle={"TODO"} />
+			<LabeledField label="Authoren">
+				<div className="flex flex-col">
+					<IconButton
+						type="button"
+						data-testid="BenoetigteSkills-add"
+						onClick={() => setSelectAuthorSelectorModal(true)}
+						title="Hinzufügen"
+						text="Hinzufügen"
+						icon={<PlusIcon className="h-5" />}
+					/>
+					{authors.length === 0 && (
+						<div className="mt-3 text-sm text-gray-500">Keine Skills vorhanden</div>
+					)}
+					<div className="mt-3 max-h-40 overflow-auto">
+						{authors.map((author, index) => (
+							<InlineRemoveButton
+								key={index}
+								label={author.displayName}
+								onRemove={() => onDeleteAuthor(author)}
+								onClick={() => {}} //TODO
+							/>
+						))}
+					</div>
+
+					{selectAuthorSelector && (
+						<SelectAuthorSelectorDialog
+							onClose={author => {
+								setSelectAuthorSelectorModal(false);
+								onAddAuthor(author);
+							}}
+						/>
+					)}
+				</div>
+			</LabeledField>
+		</>
+	);
+}
+
+export function SelectAuthorSelectorDialog({
+	onClose
+}: {
+	onClose: OnDialogCloseFn<AuthorSelector[]>;
+}) {
+	return <AuthorSelectorModal onClose={onClose} />;
+}
+
+function AuthorSelectorModal({ onClose }: { onClose: OnDialogCloseFn<AuthorSelector[]> }) {
+	const { data: authors, isLoading } = trpc.author.getAll.useQuery();
+
+	return (
+		<Dialog onClose={() => onClose(undefined)} title={"Füge die Skills hinzu"}>
+			{isLoading ? (
+				<LoadingBox />
+			) : (
+				<>
+					{authors && (
+						<SelectSelectorForm
+							onClose={onClose}
+							//skills is missing some properties here
+							authors={authors as AuthorSelector[]}
+						/>
+					)}
+				</>
+			)}
+		</Dialog>
+	);
+}
+function SelectSelectorForm({
+	onClose,
+	authors
+}: {
+	onClose: OnDialogCloseFn<AuthorSelector[]>;
+	authors: AuthorSelector[];
+}) {
+	const [search, setSearch] = useState("");
+	const [checkBoxMap, setCheckBoxMap] = useState(new Map<AuthorSelector, boolean>());
+	useEffect(() => {
+		const map = new Map<AuthorSelector, boolean>();
+		authors.forEach(author => {
+			map.set(author, false);
+		});
+		setCheckBoxMap(map);
+	}, [authors]);
+
+	const setAuthor = (author: AuthorSelector) => {
+		checkBoxMap.set(author, !checkBoxMap.get(author));
+	};
+
+	const filteredSelectors =
+		search !== ""
+			? authors.filter(skill =>
+					skill.displayName.toLowerCase().includes(search.toLowerCase())
+			  )
+			: authors;
+
+	return (
+		<>
+			<SearchField
+				placeholder="Suche nach Skills"
+				onChange={e => {
+					setSearch(e.target.value);
+				}}
+			/>
+			<div className="flex flex-col justify-between overflow-auto">
+				<section className="flex h-64 flex-col rounded-lg border border-light-border p-4">
+					<div className="flex flex-col">
+						{authors.length === 0 && <p>Keine Skills vorhanden</p>}
+						{authors.length > 0 && (
+							<>
+								{filteredSelectors
+									.sort((a, b) => a.displayName.localeCompare(b.displayName))
+									.map((skill, index) => (
+										<span
+											key={"span: " + skill.slug + index}
+											className="flex items-center gap-2"
+										>
+											<SelectorElementMemorized
+												key={skill.slug + index}
+												skill={skill}
+												value={checkBoxMap.get(skill) ?? false}
+												setSkill={setAuthor}
+											/>
+										</span>
+									))}
+							</>
+						)}
+					</div>
+				</section>
+			</div>
+			<DialogActions onClose={onClose}>
+				<button
+					className="btn-primary"
+					onClick={() => {
+						onClose(authors.filter(skill => checkBoxMap.get(skill)));
+					}}
+				>
+					Speichern
+				</button>
+			</DialogActions>
+		</>
+	);
+}
+const SelectorElementMemorized = memo(SelectorElement);
+
+function SelectorElement({
+	skill,
+	setSkill,
+	value
+}: {
+	skill: AuthorSelector;
+	setSkill: (skill: AuthorSelector) => void;
+	value: boolean;
+}) {
+	const [checked, setChecked] = useState(value);
+
+	useEffect(() => {
+		setChecked(value);
+	}, [value]);
+
+	return (
+		<>
+			<input
+				id={"checkbox:" + skill.slug}
+				type={"checkbox"}
+				className="checkbox"
+				checked={checked}
+				onChange={() => {
+					setChecked(!checked);
+					setSkill(skill);
+				}}
+			/>
+			<label htmlFor={"checkbox:" + skill.slug} className="text-sm font-semibold">
+				{skill.displayName}
+			</label>
+		</>
+	);
+}
+//----------------------------------------------- here are good things not so experimata
 function Skills() {
 	const form = useFormContext<ExtendedCourseFormValues>();
 	const {
@@ -106,23 +336,44 @@ function BasicInfo() {
 				/>
 			</LabeledField>
 
-			<LabeledField
-				label="Beschreibung (TODO: as markdown?)"
-				error={errors.subtitle?.message}
-			>
+			<LabeledField label="Beschreibung" error={errors.description?.message}>
 				<textarea {...register("description")} placeholder="" className="h-full" />
 			</LabeledField>
 
-			<LabeledField label="Fachgebiet" error={errors.subtitle?.message}>
+			<LabeledField label="Fachgebiet" error={errors.subjectId?.message}>
 				<SubjectDropDown subjects={subjects} onChange={onSubjectChange} />
 			</LabeledField>
 
-			<LabeledField label="Spezialisierung" error={errors.subtitle?.message}>
+			<LabeledField label="Spezialisierung" error={errors.specializationId?.message}>
 				<SpecializationDropDown
 					specializations={specializations}
 					onChange={onSpecializationChange}
 				/>
 			</LabeledField>
+
+			{/** TODO: who knows if it works - cannot connect to MinIO */}
+			<Controller
+				control={form.control}
+				name="imgUrl"
+				render={({ field }) => (
+					<LabeledField label="Bild" error={errors.imgUrl?.message}>
+						<div className="flex w-full gap-4">
+							<div className="flex w-full flex-col gap-2">
+								<Upload
+									mediaType="image"
+									onUploadCompleted={field.onChange}
+									preview={
+										<ImageOrPlaceholder
+											src={field.value ?? undefined}
+											className="mx-auto h-32 w-32 shrink-0 rounded-lg"
+										/>
+									}
+								/>
+							</div>
+						</div>
+					</LabeledField>
+				)}
+			></Controller>
 
 			<div className="my-5 border-t border-gray-200">
 				<AuthorsForm
@@ -191,7 +442,7 @@ function SpecializationDropDown({
 		specializations?.[0]?.specializationId ?? ""
 	);
 
-	const changeDisplaySelectedRepository = (id: string) => {
+	const changeDisplaySelectedSpecialization = (id: string) => {
 		setSelectedSpecialization(id);
 	};
 
@@ -205,7 +456,7 @@ function SpecializationDropDown({
 				className="textfield"
 				value={selectedSpecialization ?? specializations[0].specializationId}
 				onChange={e => {
-					changeDisplaySelectedRepository(e.target.value);
+					changeDisplaySelectedSpecialization(e.target.value);
 				}}
 			>
 				{specializations.map(specialization => (
@@ -220,15 +471,21 @@ function SpecializationDropDown({
 		</div>
 	);
 }
-
-function Selectors() {
+/*
+function Selectors({ authors, onChange }: { authors: Author[]; onChange: (id: string) => void }) {
 	const [openAddDialog, setOpenAddDialog] = useState(false);
 
 	const handleAdd = () => {
 		// TODO
 		setOpenAddDialog(false);
 	};
+	const [selectedSpecialization, setSelectedSpecialization] = useState<string>(
+		authors?.[0]?.displayName ?? ""
+	);
 
+	const changeDisplaySelectedRepository = (slug: string) => {
+		setSelectedSpecialization(slug);
+	};
 	return (
 		<div className="my-5">
 			<SidebarSectionTitle
@@ -245,6 +502,21 @@ function Selectors() {
 					icon={<PlusIcon className="h-5" />}
 				/>
 			</div>
+			<div className="flex flex-col">
+				<select
+					className="textfield"
+					value={selectedSpecialization ?? authors[0].slug}
+					onChange={e => {
+						changeDisplaySelectedRepository(e.target.value);
+					}}
+				>
+					{authors.map(author => (
+						<option key={author.displayName} value={author.displayName}>
+							{author.displayName}
+						</option>
+					))}
+				</select>
+			</div>
 		</div>
 	);
-}
+}*/
