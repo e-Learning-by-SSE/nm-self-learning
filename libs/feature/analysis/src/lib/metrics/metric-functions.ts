6e6c34c063ce3405e77e1b7aa6424d2712320672
Sascha El-Sharkawy <elscha@sse.uni-hildesheim.de>
2024-09-12 06:09:54 +0000

    feat(Metrics): nHints, nSuccessful, nAttempts

---
+++ libs/feature/analysis/src/lib/metrics/metric-functions.ts
@@ -0,0 +1,57 @@
+import { UserEvent } from "@self-learning/database";
+import { EventTypeKeys, EventType } from "@self-learning/types";
+
+export function isEventType<K extends EventTypeKeys>(
+	event: UserEvent,
+	type: K
+): event is UserEvent & { payload: EventType[K] } {
+	return event.type === type;
+}
+
+/**
+ * Counts the number of used hints of an array of Lesson_Quiz_Submission events.
+ *
+ * @example Usage:
+ * ```
+ * const nHints = data.filter(e => isEventType(e, "LESSON_QUIZ_SUBMISSION")).reduce(hintsUsed, 0);
+ * ```
+ * @param accumulator The number of hints used so far.
+ * @param event May only be applied to Lesson_Quiz_Submission events.
+ * @returns The number of used hints.
+ */
+export function hintsUsed(
+	accumulator: number,
+	event: UserEvent & { payload: EventType["LESSON_QUIZ_SUBMISSION"] }
+) {
+	return accumulator + event.payload.hintsUsed.length;
+}
+
+/**
+ * Counts the number of successful and failed quiz attempts of an array of Lesson_Quiz_Submission events.
+ * @example Usage:
+ * ```
+ * const { successful, failed } = events
+	    .filter(e => isEventType(e, "LESSON_QUIZ_SUBMISSION"))
+		.reduce(quizAttempts, { successful: 0, failed: 0 });
+ * ```
+ * @param accumulator The number of successful and failed quiz attempts so far.
+ * @param event May only be applied to Lesson_Quiz_Submission events.
+ * @returns The number of successful and failed quiz attempts.
+ */
+export function quizAttempts(
+	accumulator: { successful: number; failed: number },
+	event: UserEvent & { payload: EventType["LESSON_QUIZ_SUBMISSION"] }
+) {
+	// Values of the event
+	const attempts = event.payload.attempts;
+	const successfulAttempts = event.payload.solved ? 1 : 0;
+	const failedAttempts = attempts - successfulAttempts;
+
+	// Values of the accumulator
+	let { successful, failed } = accumulator;
+	successful = successful != undefined ? successful + successfulAttempts : successfulAttempts;
+	failed = failed != undefined ? failed + failedAttempts : failedAttempts;
+
+	// Return the new values
+	return { successful, failed };
+}
