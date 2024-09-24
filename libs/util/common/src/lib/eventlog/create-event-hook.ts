2a37c895232fb41d28f5165eed10c1a95e3a5ca2
Sascha El-Sharkawy <elscha@sse.uni-hildesheim.de>
2024-09-24 07:31:39 +0000

    feat(EventLog): Client-side check if events shall be logged

--- libs/util/common/src/lib/eventlog/create-event-hook.ts
+++ libs/util/common/src/lib/eventlog/create-event-hook.ts
@@ -1,16 +1,25 @@
 import { trpc } from "@self-learning/api-client";
 import { EventLog, EventTypeKeys } from "@self-learning/types";
 import { useCallback } from "react";
+import { useSession } from "next-auth/react";
 
 export function useEventLog() {
-	const { mutateAsync } = trpc.events.create.useMutation();
+	const session = useSession();
+	const user = session.data?.user;
 
-	const newEvent = useCallback(
-		async <K extends EventTypeKeys>(event: EventLog<K>) => {
-			await mutateAsync(event);
-		},
-		[mutateAsync]
-	);
+	if (user?.eventLogEnabled === true) {
+		const { mutateAsync } = trpc.events.create.useMutation();
 
-	return { newEvent };
+		const newEvent = useCallback(
+			async <K extends EventTypeKeys>(event: EventLog<K>) => {
+				await mutateAsync(event);
+			},
+			[mutateAsync]
+		);
+
+		return { newEvent };
+	} else {
+		// Return a noop function if event logging is disabled
+		return { newEvent: () => Promise.resolve() };
+	}
 }
