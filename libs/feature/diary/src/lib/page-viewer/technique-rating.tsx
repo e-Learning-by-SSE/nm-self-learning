f500fb1e9a136cf379c043ab0765d82136515814
staciwamonika <staciwam@uni-hildesheim.de>
2024-09-27 12:01:25 +0200

    change the layout of 'Lernstrategien' dialog

--- libs/feature/diary/src/lib/page-viewer/technique-rating.tsx
+++ libs/feature/diary/src/lib/page-viewer/technique-rating.tsx
@@ -67,16 +67,14 @@ export function PersonalTechniqueRatingTile({
 					onClose={() => setStrategyDialogOpen(false)}
 					className="fixed inset-0 z-10 overflow-y-auto"
 				>
-					<div className="mt-1 p-1 grid grid-rows-2 gap-0 xl:grid-rows-[500px_100px]">
-						<div className="flex h-full overflow-y-scroll">
+					<div className="grid grid-rows-[auto_80px] gap-0 xl:grid-rows-[auto_80px]">
+						<div className="mt-1 p-1 flex columns-auto">
 							<StrategyList
 								onTechniqueClick={technique => setEvalTarget(technique)}
 								strategies={strategies}
 							/>
 						</div>
-						<div className="">
-							<DialogActions onClose={() => setStrategyDialogOpen(false)} />
-						</div>
+						<DialogActions onClose={() => setStrategyDialogOpen(false)} />
 					</div>
 				</Dialog>
 			)}
@@ -121,20 +119,20 @@ function StrategyList({ strategies, onTechniqueClick }: StrategiesProps) {
 	};
 
 	return (
-		<div className="p-4 grid grid-cols-4 gap-8">
+		<div className="p-4 columns-1 sm:columns-2 md:columns-3 lg:columns-4 gap-8">
 			{strategies.map(strategy => (
 				<div key={strategy.id} className="mb-8  break-inside-avoid">
 					<div className="flex items-center justify-between mb-4 pr-4">
 						<h2 className="font-bold text-xl mr-4">{strategy.name}</h2>
 						<button onClick={() => handleInfoClick(strategy.id)}>
-							<InformationCircleIcon className="h-6 w-6 text-blue-500" />
+							<InformationCircleIcon className="h-6 w-6 text-gray-500" />
 						</button>
 					</div>
 					<ul className="pl-8 border-l-2 border-gray-300">
 						{strategy.techniques.map(technique => (
 							<li
 								key={technique.id}
-								className="flex items-center justify-between cursor-pointer text-blue-500 underline mb-2 hover:bg-gray-200"
+								className="flex items-center justify-between cursor-pointer mb-2 hover:text-green-500"
 								onClick={() => onTechniqueClick(technique)}
 							>
 								<span>{technique.name}</span>
