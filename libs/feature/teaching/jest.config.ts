fb10ddcc7ee46386086c09fa6562b7800c1896e5
spark-sse <113825305+spark-sse@users.noreply.github.com>
2024-10-08 15:39:36 +0000

    tests: migrate from ts-jest to swc/jest
    This also changes every library config. Now the configs point to the transform declaration inside the preset and do not declare another one.

--- libs/feature/teaching/jest.config.ts
+++ libs/feature/teaching/jest.config.ts
@@ -2,15 +2,5 @@
 export default {
 	displayName: "feature-teaching",
 	preset: "../../../jest.preset.js",
-	globals: {},
-	transform: {
-		"^.+\\.[tj]sx?$": [
-			"ts-jest",
-			{
-				tsconfig: "<rootDir>/tsconfig.spec.json"
-			}
-		]
-	},
-	moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
 	coverageDirectory: "../../../coverage/libs/feature/teaching"
 };
