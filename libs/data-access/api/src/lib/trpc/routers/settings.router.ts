2aaec533d0b0f205c9892b2044ba3970f0a4c19b
Marc Palfner <148242116+palfner-sse@users.noreply.github.com>
2024-10-22 17:42:15 +0200

    feat(routers): adds Event Log for learningDiary Toggle

--- libs/data-access/api/src/lib/trpc/routers/settings.router.ts
+++ libs/data-access/api/src/lib/trpc/routers/settings.router.ts
@@ -18,19 +18,38 @@ export const settingsRouter = t.router({
 			})
 		)
 		.mutation(async ({ ctx, input }) => {
-			return await database.studentSettings.upsert({
+			const currentSettings = await database.studentSettings.findUnique({
 				where: {
 					username: ctx.user.name
-				},
-				update: {
-					learningStatistics: input.settings.learningStatistics,
-					hasLearningDiary: input.settings.hasLearningDiary
-				},
-				create: {
-					username: ctx.user.name,
-					learningStatistics: input.settings.learningStatistics,
-					hasLearningDiary: input.settings.hasLearningDiary
 				}
+			});
+
+			await database.$transaction(async tx => {
+				if (currentSettings?.hasLearningDiary !== input.settings.hasLearningDiary) {
+					await tx.eventLog.create({
+						data: {
+							type: "LTB_TOGGLE",
+							payload: { enabled: input.settings.hasLearningDiary },
+							username: ctx.user.name,
+							resourceId: ctx.user.name
+						}
+					});
+				}
+
+				return await tx.studentSettings.upsert({
+					where: {
+						username: ctx.user.name
+					},
+					update: {
+						learningStatistics: input.settings.learningStatistics,
+						hasLearningDiary: input.settings.hasLearningDiary
+					},
+					create: {
+						username: ctx.user.name,
+						learningStatistics: input.settings.learningStatistics,
+						hasLearningDiary: input.settings.hasLearningDiary
+					}
+				});
 			});
 		})
 });
