7cc919c4c39ec36c7df70a87b8635d8acbffdddc
Sascha El-Sharkawy <elscha@sse.uni-hildesheim.de>
2024-10-25 03:56:25 +0000

    test(completion): Temporary debug log

--- libs/feature/completion/src/lib/mark-as-completed.ts
+++ libs/feature/completion/src/lib/mark-as-completed.ts
@@ -20,7 +20,7 @@ export async function markAsCompleted({
 			})
 		: null;
 
-	console.error("course", course, "lessonId", lessonId, "username", username);
+	console.log("Slug", courseSlug, "course", course, "lessonId", lessonId, "username", username);
 	const result = await database.completedLesson.create({
 		data: {
 			courseId: course?.courseId,
