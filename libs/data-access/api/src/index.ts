329db147b3cb984d8fb699f6501690dcc349b8d4
staciwamonika <staciwam@uni-hildesheim.de>
2024-10-28 12:59:17 +0100

    add 'authOptions' to module alias '@self-learing/api'

--- libs/data-access/api/src/index.ts
+++ libs/data-access/api/src/index.ts
@@ -2,4 +2,5 @@ export * from "./lib/trpc/app.router";
 export * from "./lib/trpc/trpc";
 export { createTrpcContext } from "./lib/trpc/context";
 export { NextAuthPage } from "./lib/auth/auth";
+export { authOptions } from "./lib/auth/auth";
 export * from "./lib/auth/with-auth-ssr";
