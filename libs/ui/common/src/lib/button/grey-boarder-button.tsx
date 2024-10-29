5b65940b24d158b53fab186a84f07a388f846a4f
SSE-Palfner <palfner-sse@gmx.de>
2024-10-29 14:19:39 +0100

    fix(GreyBoarderButton): makes text black instead of grey

--- libs/ui/common/src/lib/button/grey-boarder-button.tsx
+++ libs/ui/common/src/lib/button/grey-boarder-button.tsx
@@ -6,7 +6,7 @@ export function GreyBoarderButton(
 	return (
 		<button
 			{...props}
-			className="border-1 border-gray-150 inline-flex items-center justify-center rounded-md border bg-white px-4 py-2 font-medium text-white hover:bg-gray-100"
+			className="border-1 border-gray-150 inline-flex items-center justify-center rounded-md border bg-white px-4 py-2 font-medium text-black hover:bg-gray-100"
 		>
 			{props.children}
 		</button>
