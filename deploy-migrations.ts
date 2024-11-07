4453535bb30ec714699b53e7715398dba9f4ce02
Sascha El-Sharkawy <elscha@sse.uni-hildesheim.de>
2024-11-07 13:01:01 +0000

    fix(Migration): Concurrent migrations fix

--- deploy-migrations.ts
+++ deploy-migrations.ts
@@ -1,9 +1,7 @@
 import { execSync } from "child_process";
-import { PrismaClient } from "@prisma/client";
 import { readdirSync, existsSync, mkdirSync, renameSync, rmSync, statSync } from "fs";
 import { join } from "path";
 
-const prisma = new PrismaClient();
 const prismaPath = join(__dirname, "libs", "data-access", "database", "prisma");
 const migrationsPath = join(prismaPath, "migrations");
 const migrationsTempPath = join(prismaPath, "migrations_temp");
@@ -33,6 +31,61 @@ function cleanup() {
 	rmSync(migrationsTempPath, { recursive: true, force: true });
 }
 
+/**
+ * Applies (step-wise) the database migration.
+ * @param migration The currently applied migration
+ * @returns The status of the migration and the `stdin` output of the migration
+ */
+function migrateDatabase(migration: string) {
+	let migrationApplied = false;
+	let result = "";
+	try {
+		result = execSync(`npx prisma migrate deploy`).toString();
+		if (result.includes("No pending migrations to apply.")) {
+			console.log(`⮡ Database migration ${migration}\x1b[1m\x1b[33m already applied\x1b[0m.`);
+		} else {
+			console.log(
+				`⮡ Database migration ${migration}\x1b[1m\x1b[32m successfully applied\x1b[0m.`
+			);
+			migrationApplied = true;
+		}
+	} catch (e) {
+		console.log("⮡ Database migration \x1b[1m\x1b[31m failed:\x1b[0m\n" + e);
+		cleanup();
+		process.exit(1);
+	}
+	return { migrationApplied, result };
+}
+
+/**
+ * Tests and executes a data migration if it exists.
+ * The data migration must be placed in the migration folder and named `data-migration.ts`.
+ * Will only be executed if the database migration was applied successfully.
+ * @param migration The currently applied migration
+ * @param migrationApplied Status of the database migration
+ */
+async function migrateData(migration: string, migrationApplied: boolean) {
+	const dataMigrationFile = join(migrationsPath, migration, dataMigration);
+	if (migrationApplied && existsSync(dataMigrationFile)) {
+		console.log(`⮡ Applying\x1b[1m\x1b[34m data migration\x1b[0m`);
+		// Create a Prisma client based on current database schema
+		execSync(`npx prisma db pull`);
+		execSync(`npx prisma generate`);
+		try {
+			const module = await import(dataMigrationFile);
+			if (module.main) {
+				// Manually call and await the `main` function from the imported file
+				// Otherwise it may be executed in parallel with the next migration
+				await module.main();
+			}
+		} catch (e) {
+			console.error("⮡ Data migration \x1b[1m\x1b[31m failed:\x1b[0m\n", e);
+			cleanup();
+			process.exit(1);
+		}
+	}
+}
+
 async function main() {
 	// Step 1: Rename  Migrations folder and copy all non-migration files and folders
 	renameSync(migrationsPath, migrationsTempPath);
@@ -54,35 +107,10 @@ async function main() {
 		moveToMigrationDir(migration);
 
 		// Execute the DB migration
-		let migrationApplied = true;
-		let result = "";
-		try {
-			result = execSync(`npx prisma migrate deploy`).toString();
-			if (result.includes("No pending migrations to apply.")) {
-				console.log(
-					`⮡ Database migration ${migration}\x1b[1m\x1b[33m already applied\x1b[0m.`
-				);
-				migrationApplied = false;
-			} else {
-				console.log(
-					`⮡ Database migration ${migration}\x1b[1m\x1b[32m successfully applied\x1b[0m.`
-				);
-			}
-		} catch (e) {
-			migrationApplied = false;
-			console.log("⮡ Database migration ${migration}\x1b[1m\x1b[31m failed:\x1b[0m\n" + e);
-			cleanup();
-			process.exit(1);
-		}
+		let { migrationApplied, result } = migrateDatabase(migration);
 
 		// Apply data migration if exists
-		const dataMigrationFile = join(migrationsPath, migration, dataMigration);
-		if (migrationApplied && existsSync(dataMigrationFile)) {
-			console.log(`⮡ Applying\x1b[1m\x1b[34m data migration\x1b[0m`);
-			execSync(`npx prisma db pull`, { stdio: "inherit" });
-			execSync(`npx prisma generate`, { stdio: "inherit" });
-			await import(dataMigrationFile);
-		}
+		await migrateData(migration, migrationApplied);
 
 		if (i === migrations.length - 1) {
 			// Print log of last migration
@@ -94,12 +122,7 @@ async function main() {
 	cleanup();
 }
 
-main()
-	.then(async () => {
-		await prisma.$disconnect();
-	})
-	.catch(async e => {
-		console.error(e);
-		await prisma.$disconnect();
-		process.exit(1);
-	});
+main().catch(async e => {
+	console.error(e);
+	process.exit(1);
+});
--- libs/data-access/database/prisma/migrations/20241106045827_learning_diary/data-migration.ts
+++ libs/data-access/database/prisma/migrations/20241106045827_learning_diary/data-migration.ts
@@ -2,23 +2,16 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-async function main() {
+export async function main() {
 	await prisma.$transaction(async tx => {
-		const repositories = await tx.skillRepository.findMany({ include: { owner: true } });
+		const repositories = await tx.skillRepository.findMany({ include: { User: true } });
 		for (const repository of repositories) {
 			await tx.skillRepository.update({
 				where: { id: repository.id },
 				data: {
-					ownerName: repository.owner.name
+					ownerName: repository.User.name
 				}
 			});
 		}
 	});
 }
-
-main()
-	.catch(async e => {
-		console.error(e);
-		process.exit(1);
-	})
-	.finally(async () => await prisma.$disconnect());
