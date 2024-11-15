fab02453936acfa0ad993a39e9224c7fa339db29
spark-sse <113825305+spark-sse@users.noreply.github.com>
2024-11-15 16:47:04 +0000

    rf(database): remove duplicate data-migration and add comments to ignore compiler errors
    Compiler errors are expected since the prisma client has changed

--- libs/data-access/database/prisma/migrations/20241106045827_learning_diary/data-migration.ts
+++ libs/data-access/database/prisma/migrations/20241106045827_learning_diary/data-migration.ts
@@ -5,11 +5,17 @@ const prisma = new PrismaClient();
 async function main() {
 	try {
 		await prisma.$transaction(async tx => {
-			const repositories = await tx.skillRepository.findMany({ include: { User: true } });
+			const repositories = await tx.skillRepository.findMany({
+				include: {
+					// @ts-expect-error User was renamed to owner
+					User: true
+				}
+			});
 			for (const repository of repositories) {
 				await tx.skillRepository.update({
 					where: { id: repository.id },
 					data: {
+						// @ts-expect-error User was renamed to owner
 						ownerName: repository.User.name
 					}
 				});
