cd7fcdd10c494949780c38e3809dd940eedfb338
lukas-png <doenges@uni-hildesheim.de>
2024-11-26 20:45:08 +0000

    feat(dashboard): change card pictures

--- apps/site/pages/dashboard/index.tsx
+++ apps/site/pages/dashboard/index.tsx
@@ -33,8 +33,8 @@ import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { StudentSettings } from "@self-learning/types";
-import { TutorialSvg } from "@self-learning/ui/static";
 import { LearningDiaryEntryStatusBadge } from "@self-learning/diary";
+import { ComputerAppsSvg, GoalsSvg, LearningSvg, ReadNotesSvg, TutorialSvg } from "@self-learning/ui/static";
 
 type Student = Awaited<ReturnType<typeof getStudent>>;
 
@@ -345,26 +345,26 @@ function DashboardPage(props: Props) {
 				<div className="grid grid-cols-1 gap-8 pt-10 xl:grid-cols-2">
 					<Card
 						href="/dashboard/courseOverview"
-						imageElement={<TutorialSvg />}
+						imageElement={<ComputerAppsSvg />}
 						title="Kursübersicht"
 					/>
 					{studentSettings?.hasLearningDiary && (
 						<>
 							<Card
 								href="/learning-diary"
-								imageElement={<TutorialSvg />}
+								imageElement={<LearningSvg />}
 								title="Lerntagebucheinträge verwalten"
 							/>
 
 							<Card
 								href="/learning-diary"
-								imageElement={<TutorialSvg />}
+								imageElement={<ReadNotesSvg />}
 								title="Übersicht des Lerntagebuchs"
 							/>
 
 							<Card
 								href="/learning-diary/goals"
-								imageElement={<TutorialSvg />}
+								imageElement={<GoalsSvg />}
 								title="Lernziele verwalten"
 							/>
 						</>
--- libs/ui/static/src/index.ts
+++ libs/ui/static/src/index.ts
@@ -7,6 +7,10 @@ import { ReactComponent as PersonalInformationSvg } from "./assets/svg/teams.svg
 import { ReactComponent as TutorialSvg } from "./assets/svg/tutorial.svg";
 import { ReactComponent as StudyingSvg } from "./assets/svg/studying.svg";
 import { ReactComponent as VoidSvg } from "./assets/svg/void.svg";
+import { ReactComponent as GoalsSvg } from "./assets/svg/goals.svg";
+import { ReactComponent as LearningSvg } from "./assets/svg/learning.svg";
+import { ReactComponent as ComputerAppsSvg } from "./assets/svg/computer-apps.svg";
+import { ReactComponent as ReadNotesSvg } from "./assets/svg/read-notes.svg";
 
 export {
 	AuthorSvg,
@@ -17,5 +21,9 @@ export {
 	PersonalInformationSvg,
 	TutorialSvg,
 	StudyingSvg,
-	VoidSvg
+	VoidSvg,
+	GoalsSvg,
+	LearningSvg,
+	ComputerAppsSvg,
+	ReadNotesSvg
 };
