ee33ab08b3b2a09f228e2dc4c5f83c2210c64dc5
Sascha El-Sharkawy <elscha@sse.uni-hildesheim.de>
2024-12-06 06:04:08 +0000

    test(teaching): Refactored tests

--- apps/site/__tests__/courses/[courseSlug]/[lessonSlug]/index.spec.tsx
+++ apps/site/__tests__/courses/[courseSlug]/[lessonSlug]/index.spec.tsx
@@ -1,10 +1,9 @@
 import { getServerSideProps } from "../../../../pages/courses/[courseSlug]/[lessonSlug]/index";
 import { database } from "@self-learning/database";
-import { Author, Course, Lesson, Skill } from "@prisma/client";
-import { CourseContent } from "@self-learning/types";
 import { getServerSession } from "next-auth";
 import { createMockContext } from "../../../context-utils";
 import { compileMarkdown } from "@self-learning/markdown";
+import { createCourseMock, createLessonMock } from "../../../mock-data-utils";
 
 // Mocks getServerSession of withAuth procedure to mock User object
 // ESM support & default mock required by NextAuth
@@ -31,61 +30,23 @@ describe("getServerSideProps", () => {
 
 	describe("Authorization", () => {
 		// For te test required properties of Lesson
-		const dummySkill: Skill = {
-			id: "skill:1",
-			name: "Skill1",
-			description: "Skill1 description",
-			repositoryId: "repo:1"
-		};
-
-		const lessonMock: Partial<
-			Lesson & { authors: Pick<Author, "username">[] } & {
-				requirements: Skill[];
-			} & { teachingGoals: Skill[] }
-		> = {
+		const lessonMock = createLessonMock({
 			// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 			lessonId: mockCtx.params!.lessonId as string,
-			slug: "lesson1",
-			title: "Lesson 1",
-			subtitle: null,
-			description: null,
-			content: [
-				{
-					type: "article",
-					value: { content: null }
-				}
-			],
-			quiz: "Quiz",
-			imgUrl: "imgUrl",
-			licenseId: 1,
-			requirements: [dummySkill],
-			teachingGoals: [dummySkill],
-			authors: [{ username: "Author1" }, { username: "Author2" }],
-			selfRegulatedQuestion: null
-		};
+			authors: ["Author1", "Author2"]
+		});
 
 		// For the test required properties of Course
-		const courseMock: Partial<
-			Course & { authors: Pick<Author, "username">[] } & { content: CourseContent }
-		> = {
-			// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-			courseId: mockCtx.params!.courseId as string,
-			slug: "course1",
-			title: "Course 1",
-			description: "Course 1 description",
-			authors: [{ username: "author1" }],
+		const courseMock = createCourseMock({
+			courseId: "course1",
+			authors: ["Author1"],
 			content: [
 				{
-					title: "Chapter 1",
-					content: [
-						{
-							// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-							lessonId: lessonMock.lessonId!
-						}
-					]
+					chapterTitle: "Chapter 1",
+					lessons: [lessonMock.lessonId]
 				}
 			]
-		};
+		});
 		const redirectPage = "loginPage";
 
 		beforeEach(() => {
@@ -97,7 +58,7 @@ describe("getServerSideProps", () => {
 			global.encodeURIComponent = jest.fn().mockReturnValue("loginPage");
 		});
 
-		it("Anonymous -> Redirect to Login", async () => {
+		it("should redirect to login on anonymous access", async () => {
 			(getServerSession as jest.Mock).mockResolvedValue({
 				user: undefined
 			});
@@ -111,7 +72,7 @@ describe("getServerSideProps", () => {
 			});
 		});
 
-		it("Authenticated -> 200", async () => {
+		it("should grant access to authenticated users", async () => {
 			(getServerSession as jest.Mock).mockResolvedValue({
 				user: { name: "user1" }
 			});
