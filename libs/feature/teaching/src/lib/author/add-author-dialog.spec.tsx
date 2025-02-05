4dd0bb6a154ea88a5f6bb896bdeba14879ac0e78
SSE-Palfner <palfner-sse@gmx.de>
2025-02-05 21:29:14 +0100

    test(add-author-dialog): adds test for the bug in #267

---
+++ libs/feature/teaching/src/lib/author/add-author-dialog.spec.tsx
@@ -0,0 +1,55 @@
+import { trpc } from "@self-learning/api-client";
+import { AddAuthorDialog } from "./add-author-dialog";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+global.ResizeObserver = class {
+	observe() {}
+
+	unobserve() {}
+
+	disconnect() {}
+};
+
+jest.mock("@self-learning/api-client", () => {
+	return {
+		trpc: {
+			author: {
+				getAll: {
+					useQuery: jest.fn()
+				}
+			}
+		}
+	};
+});
+
+describe("add-author-dialog", () => {
+	describe("combobox-onChange", () => {
+		it("should return the name of the pressed author", async () => {
+			//arrange
+			const fakeAuthors = [
+				{
+					username: "mmuster",
+					displayName: "Max Mustermann",
+					slug: "muster-mann",
+					imgUrl: null
+				}
+			];
+
+			const onCloseMock = jest.fn();
+
+			(trpc.author.getAll.useQuery as jest.Mock).mockReturnValue({
+				data: fakeAuthors
+			});
+
+			render(<AddAuthorDialog open={true} onClose={onCloseMock} />);
+
+			//act
+			const button = await screen.findByTestId("author-option");
+			await userEvent.click(button);
+
+			//assert
+			expect(onCloseMock).toHaveBeenCalledWith(fakeAuthors[0]);
+		});
+	});
+});
