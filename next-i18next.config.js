e374b25ac1272b886e03dd71ea233fe30628c804
sse-palfner <palfner-sse@gmx.de>
2025-02-25 14:50:23 +0100

    test(Jenkins): redo jekinsfile changes the path accordingly

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -9,7 +9,7 @@ module.exports = {
 	/** To avoid issues when deploying to some paas (vercel...) */
 	localePath:
 		typeof window === "undefined"
-			? require("path").resolve("./apps/site/public/locales")
+			? require("path").resolve("./public/locales")
 			: "./apps/site/public/locales",
 
 	reloadOnPrerender: process.env.NEXT_PUBLIC_IS_DEMO_INSTANCE === "true"
