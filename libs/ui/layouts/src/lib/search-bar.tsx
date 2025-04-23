2064b7c2a242e5c1f0ef44d1961998e89aa19946
Hohmann-SSE <hohmann@uni-hildesheim.de>
2025-04-23 13:41:34 +0200

    (feat)cleanup code and translation added

--- libs/ui/layouts/src/lib/search-bar.tsx
+++ libs/ui/layouts/src/lib/search-bar.tsx
@@ -8,10 +8,11 @@ import {
 import { FaceFrownIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { trpc } from "@self-learning/api-client";
 import { useSession } from "next-auth/react";
-import { useState, useMemo } from "react";
+import { useState } from "react";
 import { LoadingCircle } from "@self-learning/ui/common";
 import { BookOpenIcon, AcademicCapIcon, UserIcon } from "@heroicons/react/24/outline";
-import { getSearchSections } from "./search-section";
+import { SearchResultInfo } from "./search-section";
+import { useRouter } from "next/router";
 
 export function SearchInput({
 	searchQuery,
@@ -60,12 +61,67 @@ function NoResults() {
 	);
 }
 
+function getSearchSections({
+	courses = [],
+	lessons = [],
+	authors = []
+}: {
+	courses?: SearchResultInfo[];
+	lessons?: SearchResultInfo[];
+	authors?: SearchResultInfo[];
+}) {
+	const searchSections = [
+		{
+			title: "Kurse",
+			results: courses,
+			baseLink: "courses",
+			show: !!courses
+		},
+		{
+			title: "Lerneinheiten",
+			results: lessons,
+			baseLink: "teaching/lessons/edit",
+			show: !!lessons
+		},
+		{
+			title: "Autoren",
+			results: authors,
+			baseLink: "authors",
+			show: !!authors
+		}
+	];
+
+	const allResults = searchSections.flatMap(section =>
+		section.title === "Kurse"
+			? section.results.map(result => ({
+					...result,
+					baseLink: section.baseLink,
+					type: "course"
+				}))
+			: section.title === "Lerneinheiten"
+				? section.results.map(result => ({
+						...result,
+						baseLink: section.baseLink,
+						type: "lesson"
+					}))
+				: section.title === "Autoren"
+					? section.results.map(result => ({
+							...result,
+							baseLink: section.baseLink,
+							type: "author"
+						}))
+					: section.results.map(result => ({ ...result, baseLink: section.baseLink }))
+	);
+	return allResults;
+}
+
 export function SearchBar() {
 	const isUser = useSession().status === "authenticated";
-	const [searchQuery, setSearchQuery] = useState("");
-	const search = { title: searchQuery, page: 1 };
-	const searchAuthor = { displayName: searchQuery, page: 1 };
+	const [filterText, setFilterText] = useState("");
+	const titleSearchParams = { title: filterText, page: 1 };
+	const authorSearchParams = { displayName: filterText, page: 1 };
 	const [isFocused, setIsFocused] = useState(false);
+	const router = useRouter();
 	const handleFocus = () => {
 		setIsFocused(true);
 	};
@@ -81,45 +137,24 @@ export function SearchBar() {
 
 	const [selectedResult, setSelectedResult] = useState<SearchResultItem>();
 
-	const { data: lessons, isLoading: lessonsLoading } = trpc.lesson.findMany.useQuery(search);
+	const { data: lessons, isLoading: lessonsLoading } =
+		trpc.lesson.findMany.useQuery(titleSearchParams);
 	const { data: authors, isLoading: authorsLoading } =
-		trpc.author.findMany.useQuery(searchAuthor);
-	console.log("Authors:", authors);
+		trpc.author.findMany.useQuery(authorSearchParams);
 	const authorResults = authors?.result.map(({ displayName: title, slug }) => {
 		return { title, slug };
 	});
-	const { data: courses, isLoading: coursesLoading } = trpc.course.findMany.useQuery(search);
+	const { data: courses, isLoading: coursesLoading } =
+		trpc.course.findMany.useQuery(titleSearchParams);
 
-	const searchSections = getSearchSections({
+	const searchSections: SearchResultItem[] = getSearchSections({
 		courses: courses?.result,
 		lessons: lessons?.result,
 		authors: authorResults
 	});
-	const allResults: SearchResultItem[] = useMemo(() => {
-		return searchSections.flatMap(section =>
-			section.title === "Kurse"
-				? section.results.map(result => ({
-						...result,
-						baseLink: section.baseLink,
-						type: "course"
-					}))
-				: section.title === "Lerneinheiten"
-					? section.results.map(result => ({
-							...result,
-							baseLink: section.baseLink,
-							type: "lesson"
-						}))
-					: section.title === "Autoren"
-						? section.results.map(result => ({
-								...result,
-								baseLink: section.baseLink,
-								type: "author"
-							}))
-						: section.results.map(result => ({ ...result, baseLink: section.baseLink }))
-		);
-	}, [searchSections]);
 	const isLoading = lessonsLoading || authorsLoading || coursesLoading;
-	const handleSelect = (item: { baseLink: string; title: string; slug: string }) => {
+
+	const HandleSelect = (item: { baseLink: string; title: string; slug: string }) => {
 		if (!item) {
 			return;
 		}
@@ -130,16 +165,16 @@ export function SearchBar() {
 			const url = item.baseLink.startsWith("http")
 				? `${item.baseLink}/${item.slug}`
 				: `/${item.baseLink}/${item.slug}`;
-			window.location.href = url;
+			router.push(url);
 		}
 
-		setSearchQuery("");
+		setFilterText("");
 	};
 
 	return (
 		<Combobox
 			value={selectedResult}
-			onChange={handleSelect}
+			onChange={HandleSelect}
 			as="div"
 			className="relative w-full max-w-xs hidden lg:block"
 		>
@@ -151,15 +186,15 @@ export function SearchBar() {
 					autoComplete="off"
 					placeholder={"Suchen..."}
 					className="block w-full rounded-md border-0 bg-white py-1.5 pl-10 pr-3 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-emerald-500 lg:text-sm lg:leading-6"
-					value={searchQuery}
-					onChange={e => setSearchQuery(e.target.value)}
+					value={filterText}
+					onChange={e => setFilterText(e.target.value)}
 					onFocus={handleFocus}
 					onBlur={handleBlur}
 				/>
 			</div>
 
 			<Transition
-				show={isFocused && searchQuery.length > 0 && !isLoading}
+				show={isFocused && filterText.length > 0 && !isLoading}
 				as="div"
 				enter="transition ease-out duration-100"
 				enterFrom="transform opacity-0 scale-95"
@@ -171,8 +206,8 @@ export function SearchBar() {
 				<ComboboxOptions className="absolute z-10 mt-2 w-full origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
 					{isLoading ? (
 						<LoadingCircle className="h-5 w-5 mx-auto" />
-					) : allResults.length > 0 && isUser ? (
-						allResults.slice(0, 12).map((result, index) => (
+					) : searchSections.length > 0 && isUser ? (
+						searchSections.slice(0, 12).map((result, index) => (
 							<ComboboxOption
 								key={index}
 								value={result}
