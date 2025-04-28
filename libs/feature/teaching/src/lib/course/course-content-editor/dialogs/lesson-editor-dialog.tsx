225c8c99088df5ea44f97d9f95363b1cd7115610
SSE-Palfner <palfner-sse@gmx.de>
2025-04-28 18:28:15 +0200

    fix(lesson-editor-dialog): adds base path to "Im separaten Editor öffnen" button

--- libs/feature/teaching/src/lib/course/course-content-editor/dialogs/lesson-editor-dialog.tsx
+++ libs/feature/teaching/src/lib/course/course-content-editor/dialogs/lesson-editor-dialog.tsx
@@ -1,6 +1,8 @@
 import { Dialog, OnDialogCloseFn } from "@self-learning/ui/common";
 import { Unauthorized, useRequiredSession } from "@self-learning/ui/layouts";
 import { LessonEditor, LessonFormModel } from "@self-learning/teaching";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 export function LessonEditorDialogWithGuard({
 	onClose,
@@ -38,6 +40,8 @@ function LessonEditorDialog({
 	onClose: OnDialogCloseFn<LessonFormModel>;
 	initialLesson?: LessonFormModel;
 }) {
+	const router = useRouter();
+
 	return (
 		<Dialog
 			title={!initialLesson ? "Neue Lerneinheit erstellen" : "Lerneinheit anpassen"}
@@ -45,19 +49,19 @@ function LessonEditorDialog({
 			style={{ height: "80vh", width: "80vw" }}
 		>
 			<div className="absolute right-8 top-8 flex gap-4">
-				<a
+				<Link
 					className="btn-stroked focus:primary hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2"
 					target="_blank"
 					rel="noreferrer"
 					href={
 						initialLesson
-							? `/teaching/lessons/edit/${initialLesson?.lessonId}`
-							: `/teaching/lessons/create`
+							? `/${router.basePath}/teaching/lessons/edit/${initialLesson?.lessonId}`
+							: `/${router.basePath}/teaching/lessons/create`
 					}
 					title="Formular in einem neuen Tab öffnen. Änderungen werden nicht übernommen."
 				>
 					Im separaten Editor öffnen
-				</a>
+				</Link>
 			</div>
 			<LessonEditor onSubmit={onClose} initialLesson={initialLesson} isFullScreen={false} />
 		</Dialog>
