361875a799a390b41abd2efcdaf7ab85d4543a5d
SSE-Palfner <palfner-sse@gmx.de>
2025-04-30 15:39:45 +0200

    fix(combobox-menu): fixes width of the dropdown part

--- libs/ui/common/src/lib/dropdown-menu/combobox-menu.tsx
+++ libs/ui/common/src/lib/dropdown-menu/combobox-menu.tsx
@@ -6,7 +6,7 @@ import {
 	ComboboxButton
 } from "@headlessui/react";
 import { ChevronUpDownIcon } from "@heroicons/react/20/solid";
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export function ComboboxMenu({
 	title,
@@ -24,6 +24,13 @@ export function ComboboxMenu({
 	options: string[];
 }) {
 	const menuRef = useRef<HTMLDivElement>(null);
+	const [menuWidth, setMenuWidth] = useState<number | null>(null);
+
+	useEffect(() => {
+		if (menuRef.current) {
+			setMenuWidth(menuRef.current.offsetWidth);
+		}
+	}, []);
 
 	return (
 		<Combobox
@@ -32,10 +39,13 @@ export function ComboboxMenu({
 			value={value}
 			className="relative inline-block w-full text-left"
 		>
-			<div className="inline-flex w-full items-center rounded-md border px-4 py-2" ref={menuRef}>
+			<div
+				className="inline-flex w-full items-center rounded-md border px-4 py-2"
+				ref={menuRef}
+			>
 				<ComboboxInput
 					title={title}
-					displayValue={() => value ?? "" }
+					displayValue={() => value ?? ""}
 					onChange={e => onChange(e.target.value)}
 					className="w-full outline-none"
 				/>
@@ -44,31 +54,31 @@ export function ComboboxMenu({
 				</ComboboxButton>
 			</div>
 
-			<ComboboxOptions
-				anchor={dropdownPosition}
-				style={{
-					minWidth: menuRef.current?.offsetWidth || "auto",
-				}}
-				className="z-10 mt-1 rounded-md bg-white shadow-lg overflow-auto text-sm"
-			>
-				{options.map((option, i) => (
-					<ComboboxOption key={i} value={option}>
-						{({ focus, selected }) => (
-							<div
-								className={`w-full text-left px-3 py-1 flex items-center cursor-default ${
-									customFocusStyle
-										? customFocusStyle(focus)
-										: focus
-											? "bg-emerald-500 text-white"
-											: ""
-								} ${selected ? "font-medium" : ""}`}
-							>
-								{option}
-							</div>
-						)}
-					</ComboboxOption>
-				))}
-			</ComboboxOptions>
+			{menuWidth && (
+				<ComboboxOptions
+					anchor={dropdownPosition}
+					style={{ minWidth: menuWidth }}
+					className="z-10 rounded-md bg-white shadow-lg overflow-auto text-sm"
+				>
+					{options.map((option, i) => (
+						<ComboboxOption key={i} value={option}>
+							{({ focus, selected }) => (
+								<div
+									className={`w-full text-left px-3 py-1 flex items-center cursor-default ${
+										customFocusStyle
+											? customFocusStyle(focus)
+											: focus
+												? "bg-emerald-500 text-white"
+												: ""
+									} ${selected ? "font-medium" : ""}`}
+								>
+									{option}
+								</div>
+							)}
+						</ComboboxOption>
+					))}
+				</ComboboxOptions>
+			)}
 		</Combobox>
 	);
 }
