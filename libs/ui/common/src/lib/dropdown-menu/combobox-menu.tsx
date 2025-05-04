f05c0767c71982ff9e0e6b29e6ca362dbbceef2f
SSE-Palfner <palfner-sse@gmx.de>
2025-05-04 19:25:12 +0200

    fix(combobox-menu): fixes input border colors

--- libs/ui/common/src/lib/dropdown-menu/combobox-menu.tsx
+++ libs/ui/common/src/lib/dropdown-menu/combobox-menu.tsx
@@ -51,7 +51,7 @@ export function ComboboxMenu({
 					title={title}
 					displayValue={() => value ?? ""}
 					onChange={e => setQuery(e.target.value)}
-					className="w-full outline-none"
+					className="w-full focus:outline-none focus:ring-0 focus:ring-offset-0 focus:shadow-none focus:border-gray-300 border-gray-300"
 				/>
 				<ComboboxButton className="ml-2 text-gray-500 hover:text-gray-700">
 					<ChevronUpDownIcon className="w-5 h-5" />
--- libs/ui/common/src/lib/dropdown-menu/dropdown-menu.tsx
+++ libs/ui/common/src/lib/dropdown-menu/dropdown-menu.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment, ReactNode, useRef } from "react";
+import React, { Fragment, ReactNode, useEffect, useRef, useState } from "react";
 import { Menu, MenuButton, MenuItem, MenuItems, Transition } from "@headlessui/react";
 
 export function DropdownMenu({
@@ -14,8 +14,16 @@ export function DropdownMenu({
 	button: ReactNode;
 	children: ReactNode;
 }) {
-	const childrenArray = React.Children.toArray(children);
 	const buttonRef = useRef<HTMLButtonElement>(null);
+	const [menuWidth, setMenuWidth] = useState<number | null>(null);
+
+	const childrenArray = React.Children.toArray(children);
+
+	useEffect(() => {
+		if (buttonRef.current) {
+			setMenuWidth(buttonRef.current.offsetWidth);
+		}
+	}, []);
 
 	return (
 		<Menu as="div" className="relative inline-block text-left w-fit">
@@ -37,33 +45,35 @@ export function DropdownMenu({
 					leaveFrom="opacity-100 scale-100"
 					leaveTo="opacity-0 scale-95"
 				>
-					<MenuItems
-						anchor={dropdownPosition}
-						style={{
-							minWidth: buttonRef.current?.offsetWidth || "auto",
-						}}
-						className="absolute z-10 rounded-md bg-white shadow-lg overflow-hidden"
-					>
-						<div className="py-1 max-h-48 overflow-auto text-sm">
-							{childrenArray.map((element, i) => (
-								<MenuItem key={i}>
-									{({ focus }) => (
-										<div
-											className={`w-full text-left px-3 py-1 ${
-												customFocusStyle
-													? customFocusStyle(focus)
-													: focus
-														? "bg-emerald-500 text-white"
-														: ""
-											} flex items-center`}
-										>
-											{element}
-										</div>
-									)}
-								</MenuItem>
-							))}
-						</div>
-					</MenuItems>
+					{menuWidth && (
+						<MenuItems
+							anchor={dropdownPosition}
+							style={{
+								minWidth: buttonRef.current?.offsetWidth || "auto"
+							}}
+							className="absolute z-10 rounded-md bg-white shadow-lg overflow-hidden"
+						>
+							<div className="py-1 max-h-48 overflow-auto text-sm">
+								{childrenArray.map((element, i) => (
+									<MenuItem key={i}>
+										{({ focus }) => (
+											<div
+												className={`w-full text-left px-3 py-1 ${
+													customFocusStyle
+														? customFocusStyle(focus)
+														: focus
+															? "bg-emerald-500 text-white"
+															: ""
+												} flex items-center`}
+											>
+												{element}
+											</div>
+										)}
+									</MenuItem>
+								))}
+							</div>
+						</MenuItems>
+					)}
 				</Transition>
 			</>
 		</Menu>
