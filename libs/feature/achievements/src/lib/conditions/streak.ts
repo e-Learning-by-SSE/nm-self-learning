79976642f406e7b92811d5a2057e149723f849cb
spark-sse <113825305+spark-sse@users.noreply.github.com>
2025-06-23 12:31:15 +0000

    fix(achievements): remove streak progression on every check
    Before, every earn-check would lead to an update on the database table which then counts as progress-made. This was shown to the user in their dashboard which was confusing since no real progress was made.
    
    Now only decreases or increases in streak count as a update

--- libs/feature/achievements/src/lib/conditions/streak.ts
+++ libs/feature/achievements/src/lib/conditions/streak.ts
@@ -8,12 +8,28 @@ export const checkDailyStreak: ConditionChecker = async (achievement, username, 
 	const data = await database.gamificationProfile.findUnique({
 		where: { username },
 		select: {
-			loginStreak: true
+			loginStreak: true,
+			achievementProgress: {
+				where: { achievementId: achievement.id },
+				select: { progressValue: true }
+			}
 		}
 	});
 	if (!data) return {};
 
-	return {
-		newValue: (data.loginStreak as unknown as LoginStreak).count
-	};
+	const loginStreak = data.loginStreak as unknown as LoginStreak;
+	let newValue: number | undefined;
+
+	if (data.achievementProgress.length > 0) {
+		// bereits Fortschritt vorhanden
+		const progress = data.achievementProgress[0];
+		if (progress.progressValue !== loginStreak.count) {
+			console.log(
+				`Achievement check for daily_streak: ${username} has a streak of ${loginStreak.count} days, but progress is ${progress.progressValue} days`
+			);
+			// Fortschritt hat sich geändert
+			newValue = loginStreak.count; // decrease möglich; immer aktuellsten Wert nehmen
+		}
+	}
+	return { newValue };
 };
