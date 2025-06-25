2ac9b27a6f5a96c25cbd7f0d98ca77a51175cbb8
spark-sse <113825305+spark-sse@users.noreply.github.com>
2025-06-25 09:17:00 +0000

    fix(prisma): add creation of gamificationProfiles for

--- libs/data-access/database/prisma/migrations/20250624140837_gamification_add_expand/data-migration.ts
+++ libs/data-access/database/prisma/migrations/20250624140837_gamification_add_expand/data-migration.ts
@@ -1,36 +1,90 @@
-import { PrismaClient } from "@prisma/client";
+import { NotificationChannel, NotificationType, Prisma, PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
+export function getDefaultNotificationData(defaultValue?: boolean) {
+	const types = Object.values(NotificationType);
+	const channels = Object.values(NotificationChannel);
+	return types.flatMap(type =>
+		channels.map(channel => ({
+			type,
+			channel,
+			enabled: defaultValue // if undefined -> prisma default
+		}))
+	);
+}
+
+export async function createInitialNotificationSettings(
+	user: { id: string },
+	client: Prisma.TransactionClient | PrismaClient
+) {
+	await client.userNotificationSetting.createMany({
+		data: getDefaultNotificationData().map(setting => ({
+			...setting,
+			userId: user.id
+		}))
+	});
+}
+
+export async function createNewProfile(
+	username: string,
+	tx: PrismaClient | Prisma.TransactionClient
+) {
+	const newStreak = {
+		count: 1,
+		status: "active",
+		pausedUntil: null
+	};
+
+	await tx.gamificationProfile.create({
+		data: {
+			user: { connect: { name: username } },
+			username,
+			lastLogin: new Date(),
+			// energy: 2, // default handles by prisma
+			loginStreak: newStreak
+		}
+	});
+}
+
 async function main() {
-    try {
-        await prisma.$transaction(async tx => {
-            const userSettings = await tx.user.findMany({});
+	try {
+		await prisma.$transaction(async tx => {
+			const userSettings = await tx.user.findMany({
+				include: {
+					gamificationProfile: true
+				}
+			});
 
-            const promises = userSettings.map(async user => {
-                return tx.features.upsert({
-                    where: { userId: user.id },
-                    create: {
-                        userId: user.id,
-                        username: user.name,
-                        learningDiary: user.enabledFeatureLearningDiary,
-                        learningStatistics: user.enabledLearningStatistics
-                    },
-                    update: {
-                        learningDiary: user.enabledFeatureLearningDiary,
-                        learningStatistics: user.enabledLearningStatistics
-                    }
-                });
-            });
+			const promises = userSettings.map(async user => {
+				if (!user.gamificationProfile) {
+					// since we introduce this newly, users should never already have gamification profiles
+					await createNewProfile(user.name, tx);
+				}
+				await createInitialNotificationSettings(user, tx);
+				return tx.features.upsert({
+					where: { userId: user.id },
+					create: {
+						userId: user.id,
+						username: user.name,
+						learningDiary: user.enabledFeatureLearningDiary,
+						learningStatistics: user.enabledLearningStatistics
+					},
+					update: {
+						learningDiary: user.enabledFeatureLearningDiary,
+						learningStatistics: user.enabledLearningStatistics
+					}
+				});
+			});
 
-            await Promise.all(promises);
-        });
-    } finally {
-        await prisma.$disconnect();
-    }
+			await Promise.all(promises);
+		});
+	} finally {
+		await prisma.$disconnect();
+	}
 }
 
 main().catch(e => {
-    console.error(e);
-    process.exit(1);
+	console.error(e);
+	process.exit(1);
 });
