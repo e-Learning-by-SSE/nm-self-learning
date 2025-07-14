a1418aaf779534d80f9cafd7d81f9ef5a9fde540
archql <artem.drankevich@gmail.com>
2025-07-14 21:21:11 +0000

    rf(draggable): add components to create infinite scroll with the outline navigation

---
+++ libs/ui/layouts/src/lib/draggable/draggable.tsx
@@ -0,0 +1,281 @@
+"use client";
+
+import { DragDropContext, Draggable, Droppable } from "@hello-pangea/dnd";
+import { Bars3Icon } from "@heroicons/react/24/outline";
+import { RemovableTab, Tab, Tabs, XButton } from "@self-learning/ui/common";
+import { useEffect, useRef } from "react";
+
+/**
+ * @usage
+ * - Select combination of two elements: Viewer and Outline/Selector. You can use Outline/Selector separately from viewer
+ * - T must have a string id field.
+ * - Create your container of T[].
+ * - Create TWO useState<number | undefined>(undefined):
+ *   - activeIndex - holds current viewed element
+ *   - targetIndex - used to trigger scroll on click on the desired content
+ * - RenderContent for Viewer is a view for each T. Must have item: T and index: number fields.
+ *   - if supplied item is undefined => content array is empty
+ * - RenderContent for Outline/Selector is a view for each T. Must have item: T.
+ *   - Keep it simple! Ideally make it just text
+ *   - if supplied item is undefined => content array is empty
+ * !!! look carefully which indexes are supplied into components! Can cause nasty errors!
+ *
+ * resetTargetIndex - set it to `() => setTargetIndex(nullptr)`
+ */
+export function DraggableContentViewer<T extends { id: string }>({
+	content,
+	setActiveIndex,
+	targetIndex, // separate from activeIndex to avoid circle dependency
+	resetTargetIndex,
+	RenderContent
+}: {
+	content: T[];
+	setActiveIndex: (idx: number | undefined) => void;
+	targetIndex: number | undefined;
+	resetTargetIndex: () => void;
+	RenderContent: (props: { item?: T; index?: number }) => JSX.Element;
+}) {
+	const contentRefs = useRef<(HTMLDivElement | null)[]>([]);
+	const visibleItemsRef = useRef<Set<number>>(new Set());
+	// scroll into selected
+	useEffect(() => {
+		if (targetIndex !== undefined && content[targetIndex] && contentRefs.current[targetIndex]) {
+			contentRefs.current[targetIndex].scrollIntoView({
+				behavior: "smooth",
+				block: "start"
+			});
+			resetTargetIndex();
+		}
+	}, [targetIndex]); // Deliberately avoid update on content to avoid scroll on delete, on swap, .... Allow only on click
+	// Detect active element
+	const calculateMinVisibleIndex = () => {
+		if (visibleItemsRef.current.size === 0) return undefined;
+		let min: number | undefined;
+		for (const idx of visibleItemsRef.current) {
+			if (min === undefined || idx < min) min = idx;
+		}
+		return min;
+	};
+	useEffect(() => {
+		const remInPx = parseInt(getComputedStyle(document.documentElement).fontSize);
+		console.log(`${4 * remInPx}px 0px 0px 0px`);
+
+		const observer = new IntersectionObserver(
+			entries => {
+				entries.forEach(entry => {
+					const indexStr = (entry.target as HTMLElement).dataset?.["activeIndex"];
+					if (indexStr !== undefined) {
+						const index = parseInt(indexStr, 10);
+						if (entry.isIntersecting) {
+							visibleItemsRef.current.add(index);
+						} else {
+							visibleItemsRef.current.delete(index);
+						}
+					}
+				});
+				const newActiveIndex = calculateMinVisibleIndex();
+				setActiveIndex(newActiveIndex); // React ignores same index update
+			},
+			{
+				root: null,
+				rootMargin: `-${4 * remInPx}px 0px 0px 0px`,
+				threshold: 0.01
+			}
+		);
+
+		contentRefs.current.forEach(ref => {
+			if (ref) {
+				observer.observe(ref);
+			}
+		});
+
+		return () => {
+			observer.disconnect();
+		};
+	}, [content, setActiveIndex]);
+
+	if (!content || content.length === 0) {
+		return (
+			<div>
+				<RenderContent />
+			</div>
+		);
+	}
+
+	return (
+		<div className="flex flex-col gap-4">
+			{content.map((item, index) => (
+				<div
+					key={item.id}
+					ref={el => {
+						contentRefs.current[index] = el;
+					}}
+					// Add a data attribute to easily retrieve the index in the Intersection Observer callback
+					data-active-index={index}
+					// Scroll positioning fix because of website header
+					className="scroll-mt-16"
+				>
+					<RenderContent index={index} item={item} />
+				</div>
+			))}
+		</div>
+	);
+}
+
+export function DraggableContentOutline<T extends { id: string }>({
+	content,
+	swapContent,
+	removeContent,
+	activeIndex,
+	setTargetIndex,
+	RenderContent
+}: {
+	content: T[];
+	swapContent: (src: number, dest: number) => void;
+	removeContent?: (idx: number) => void;
+	activeIndex: number | undefined;
+	setTargetIndex: (idx: number) => void; // separate from activeIndex to avoid circle dependency
+	RenderContent: (props: { item?: T }) => JSX.Element;
+}) {
+	return (
+		<DragDropContext
+			onDragEnd={result => {
+				if (result.reason !== "DROP" || !result.destination) return;
+				swapContent(result.source.index, result.destination.index);
+			}}
+		>
+			<Droppable droppableId="droppable" direction="vertical">
+				{provided => (
+					<div
+						ref={provided.innerRef}
+						{...provided.droppableProps}
+						className="overflow-y-auto"
+					>
+						<div className="flex flex-col flex-no-wrap gap-4 min-w-max">
+							{content.map((item, index) => (
+								<Draggable key={item.id} draggableId={item.id} index={index}>
+									{provided => (
+										<div
+											ref={provided.innerRef}
+											{...provided.draggableProps}
+											{...provided.dragHandleProps}
+											className={`flex gap-5 text-nowrap flex-nowrap items-center ${activeIndex === index ? "text-secondary" : "text-light"}`}
+										>
+											<Bars3Icon className="h-5 text-light" />
+											<span onClick={() => setTargetIndex(index)}>
+												<RenderContent item={item} />
+											</span>
+											{removeContent && (
+												<XButton
+													onClick={() => removeContent(index)}
+													title="Entfernen"
+													className="flex items-center"
+												/>
+											)}
+										</div>
+									)}
+								</Draggable>
+							))}
+							{(!content || content.length === 0) && (
+								<div className={`flex gap-5 text-nowrap flex-nowrap items-center`}>
+									<span>
+										<RenderContent />
+									</span>
+								</div>
+							)}
+						</div>
+						{provided.placeholder}
+					</div>
+				)}
+			</Droppable>
+		</DragDropContext>
+	);
+}
+
+export function DraggableContentSelector<T extends { id: string }>({
+	content,
+	swapContent,
+	removeContent,
+	activeIndex,
+	selectContentIndex,
+	RenderContent
+}: {
+	content: T[];
+	swapContent: (src: number, dest: number) => void;
+	removeContent?: (idx: number) => void;
+	activeIndex: number | undefined;
+	selectContentIndex: (idx: number) => void;
+	RenderContent: (props: { item?: T }) => JSX.Element;
+}) {
+	return (
+		<DragDropContext
+			onDragEnd={result => {
+				if (result.reason !== "DROP" || !result.destination) return;
+				swapContent(result.source.index, result.destination.index);
+			}}
+		>
+			<Droppable droppableId="droppable" direction="horizontal">
+				{provided => (
+					<div
+						ref={provided.innerRef}
+						{...provided.droppableProps}
+						className="overflow-auto"
+					>
+						<Tabs selectedIndex={activeIndex} onChange={selectContentIndex}>
+							<div className="flex flex-no-wrap gap-4 min-w-max">
+								{content.map((item, index) => (
+									<Draggable key={item.id} draggableId={item.id} index={index}>
+										{provided => (
+											<div
+												ref={provided.innerRef}
+												{...provided.draggableProps}
+												{...provided.dragHandleProps}
+											>
+												{removeContent && (
+													<RemovableTab
+														onRemove={() => removeContent(index)}
+													>
+														<div className="flex gap-5 items-center">
+															<Bars3Icon className="h-5 text-light" />
+															<RenderContent item={item} />
+														</div>
+													</RemovableTab>
+												)}
+												{!removeContent && (
+													<Tab>
+														<div className="flex gap-5 items-center">
+															<Bars3Icon className="h-5 text-light" />
+															<RenderContent item={item} />
+														</div>
+													</Tab>
+												)}
+											</div>
+										)}
+									</Draggable>
+								))}
+								{(!content || content.length === 0) && (
+									<Draggable key={"null"} draggableId={"null"} index={0}>
+										{provided => (
+											<div
+												ref={provided.innerRef}
+												{...provided.draggableProps}
+												{...provided.dragHandleProps}
+												className={`flex gap-5 text-nowrap flex-nowrap items-center`}
+											>
+												<Bars3Icon className="h-5 text-light" />
+												<span>
+													<RenderContent />
+												</span>
+											</div>
+										)}
+									</Draggable>
+								)}
+							</div>
+						</Tabs>
+						{provided.placeholder}
+					</div>
+				)}
+			</Droppable>
+		</DragDropContext>
+	);
+}
