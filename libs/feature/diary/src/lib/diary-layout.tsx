74848a19378d6d744b6564f9af53d574b4094d70
lukas-png <65257500+lukas-png@users.noreply.github.com>
2025-09-03 16:45:55 +0200

    feat(lesson,diary): optimize sidebar menus for mobile view (#308)

--- libs/feature/diary/src/lib/diary-layout.tsx
+++ libs/feature/diary/src/lib/diary-layout.tsx
@@ -1,6 +1,7 @@
 import { NextComponentType, NextPageContext } from "next";
-import { Sidebar } from "./page-viewer/page-sidebar";
+import { DiarySidebarMobile, Sidebar } from "./page-viewer/page-sidebar";
 import { PagesMeta, Strategy } from "./access-learning-diary";
+import { PageChanger } from "./page-viewer/page-changer";
 
 interface DiaryProps {
 	diaryId: string;
@@ -12,15 +13,34 @@ export function DiaryLayout(
 	Component: NextComponentType<NextPageContext, unknown, DiaryProps>,
 	pageProps: DiaryProps
 ) {
-	
 	return (
 		<div className="flex flex-col">
 			<div className="mx-auto flex w-full flex-col gap-8 px-4 sm:flex-col-reverse xl:grid xl:grid-cols-[400px_1fr]">
-				<div className="w-full">
-					<Sidebar selectedPageId={pageProps.diaryId} pages={pageProps.pages} />
+				<div className="hidden xl:block">
+					<div className="w-full">
+						<Sidebar selectedPageId={pageProps.diaryId} pages={pageProps.pages} />
+					</div>
 				</div>
-				<div className="w-full pt-8 pb-16">
-					<Component {...pageProps} />
+				<div className="xl:hidden">
+					<DiarySidebarMobile
+						selectedPageId={pageProps.diaryId}
+						pages={pageProps.pages}
+					/>
+				</div>
+				<div className="w-full xl:pt-8 lg:w-2/3 pb-16 mx-auto">
+					<div className="hidden xl:block">
+						<div className="flex mb-4 mt-4 justify-center">
+							<PageChanger
+								key={pageProps.diaryId}
+								pages={pageProps.pages}
+								currentPageId={pageProps.diaryId}
+							/>
+						</div>
+					</div>
+
+					<div className="flex justify-center">
+						<Component {...pageProps} />
+					</div>
 				</div>
 			</div>
 		</div>
