a7a8626790026a82d318aaa3da2bd75d87cbde11
malikhunain <hunain.html@gmail.com>
2025-09-04 16:36:05 +0000

    feat(ai-tutor): create test suites for ai-tutor

--- libs/feature/ai-tutor/jest.config.ts
+++ libs/feature/ai-tutor/jest.config.ts
@@ -4,7 +4,10 @@ export default {
 	preset: "../../../jest.preset.js",
 	transform: {
 		"^(?!.*\\.(js|jsx|ts|tsx|css|json)$)": "@nx/react/plugins/jest",
-		"^.+\\.[tj]sx?$": ["babel-jest", { presets: ["@nx/react/babel"] }]
+		"^.+\\.[tj]sx?$": [
+			"babel-jest",
+			{ presets: ["@nx/react/babel"], configFile: "./libs/feature/ai-tutor/.babelrc" }
+		]
 	},
 	moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
 	coverageDirectory: "../../../coverage/libs/feature/ai-tutor"
