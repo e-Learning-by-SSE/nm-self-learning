4111f996f68dad9c7355572fc193fd0971ae4fb2
Sascha El-Sharkawy <elscha@sse.uni-hildesheim.de>
2025-09-19 02:43:49 +0000

    rf(api): More concise implementation of withTranslations + tests

--- libs/data-access/api/src/lib/internationalization/withTranslation.ts
+++ libs/data-access/api/src/lib/internationalization/withTranslation.ts
@@ -4,41 +4,36 @@ import { SSRConfig } from "next-i18next";
 
 export function withTranslations<P extends object = object>(
 	namespaces: string[],
-	handler?: (
+	handler: (
 		context: GetServerSidePropsContext
-	) => Promise<GetServerSidePropsResult<P>> | GetServerSidePropsResult<P>
+	) => Promise<GetServerSidePropsResult<P>> | GetServerSidePropsResult<P> = async () => ({
+		props: {} as P
+	})
 ): (context: GetServerSidePropsContext) => Promise<GetServerSidePropsResult<P & SSRConfig>> {
 	return async function getServerSidePropsWithTranslations(
 		context: GetServerSidePropsContext
 	): Promise<GetServerSidePropsResult<P & SSRConfig>> {
+		const result = await handler(context);
+
+		// Return "not found" and "redirect" results as is
+		if (!("props" in result)) {
+			return result;
+		}
+
+		// HOF primarily responsible for passing translations into the page
 		const { locale } = context;
 		const translations = (await serverSideTranslations(
 			locale ?? "en",
 			namespaces
 		)) as SSRConfig;
 
-		if (!handler) {
-			return {
-				props: {
-					...translations
-				}
-			} as GetServerSidePropsResult<P & SSRConfig>;
-		}
-
-		const result = await handler(context);
-
-		if ("props" in result) {
-			const mergedProps = {
+		// Merge translations with the original props from the handler
+		return {
+			...result,
+			props: {
 				...result.props,
 				...translations
-			} as P & SSRConfig;
-
-			return {
-				...result,
-				props: mergedProps
-			} as GetServerSidePropsResult<P & SSRConfig>;
-		}
-
-		return result as GetServerSidePropsResult<P & SSRConfig>;
+			}
+		} as GetServerSidePropsResult<P & SSRConfig>;
 	};
 }
