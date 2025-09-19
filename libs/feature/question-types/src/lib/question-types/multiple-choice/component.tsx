500dee2b036b7a22c6224186608548ce5212ce39
Sascha El-Sharkawy <elscha@sse.uni-hildesheim.de>
2025-09-19 11:58:48 +0000

    feat(quiz): Merges Justification & Multiple Choice question to one view
    Unclear if we really should remove the Justification part of MultipleChoiceAnswer. I deleted it as this became dead code after revision. However, this may become relevant in future when integrating AI into the platform.
    
    Closes #435

--- libs/feature/question-types/src/lib/question-types/multiple-choice/component.tsx
+++ libs/feature/question-types/src/lib/question-types/multiple-choice/component.tsx
@@ -1,40 +1,30 @@
 import { useQuiz } from "@self-learning/quiz";
 import { MarkdownContainer } from "@self-learning/ui/layouts";
 import { MDXRemote } from "next-mdx-remote";
-import { PropsWithChildren, useState } from "react";
+import { PropsWithChildren } from "react";
 import { Feedback } from "../../feedback";
 import { useQuestion } from "../../use-question-hook";
 import { LessonLayoutProps } from "@self-learning/lesson";
-import { LessonType } from "@prisma/client";
 
-export default function MultipleChoiceAnswer({
-	questionStep,
-	lesson
-}: {
-	questionStep: number;
-	lesson: LessonLayoutProps["lesson"];
-}) {
+export default function MultipleChoiceAnswer({ lesson }: { lesson: LessonLayoutProps["lesson"] }) {
 	const { question, setAnswer, answer, markdown, evaluation } = useQuestion("multiple-choice");
-	const [justifiedAnswer, setJustifiedAnswer] = useState(JSON.parse(JSON.stringify(answer)));
 	const { config } = useQuiz();
-
-	const isJustified = questionStep === 1 && lesson.lessonType === LessonType.SELF_REGULATED;
 
 	return (
 		<>
 			<section className="flex flex-col gap-4">
-			{Object.entries(answer.value).map(([answerId]) => (
+				{Object.entries(answer.value).map(([answerId]) => (
 					<MultipleChoiceOption
 						key={answerId}
 						disabled={!!evaluation}
 						showResult={!!evaluation && config.showSolution}
 						isUserAnswerCorrect={evaluation?.answers[answerId] === true}
-						isCorrect={question.answers.find(a => a.answerId === answerId)?.isCorrect ?? false}
-						isSelected={
-							(isJustified ? justifiedAnswer : answer).value[answerId] === true
+						isCorrect={
+							question.answers.find(a => a.answerId === answerId)?.isCorrect ?? false
 						}
+						isSelected={answer.value[answerId] === true}
 						onToggle={() => {
-							const answerSetter = isJustified ? setJustifiedAnswer : setAnswer;
+							const answerSetter = setAnswer;
 							answerSetter(old => ({
 								...old,
 								value: {
@@ -43,7 +33,6 @@ export default function MultipleChoiceAnswer({
 								}
 							}));
 						}}
-						justifyChoice={isJustified}
 					>
 						{markdown.answersMd[answerId] ? (
 							<MarkdownContainer>
@@ -67,15 +56,13 @@ export function MultipleChoiceOption({
 	isSelected,
 	isUserAnswerCorrect,
 	onToggle,
-	disabled,
-	justifyChoice
+	disabled
 }: PropsWithChildren<{
 	showResult: boolean;
 	isSelected: boolean;
 	isCorrect: boolean;
 	isUserAnswerCorrect: boolean;
 	disabled: boolean;
-	justifyChoice: boolean;
 	onToggle: () => void;
 }>) {
 	let className = "bg-white";
@@ -84,34 +71,6 @@ export function MultipleChoiceOption({
 		className = isUserAnswerCorrect
 			? "bg-emerald-50 border-emerald-500"
 			: "bg-red-50 border-red-500";
-	}
-
-	if (justifyChoice) {
-		return (
-			<div className="rounded-lg border border-light-border bg-white">
-				<button
-					className={`flex w-full gap-8 rounded-t-lg border-b border-light-border py-2 px-8 text-start focus:outline-secondary ${className}`}
-					onClick={onToggle}
-					disabled={disabled}
-					data-testid="MultipleChoiceOption"
-				>
-					<input
-						type={"checkbox"}
-						checked={isSelected}
-						onChange={() => {
-							/** Bubbles up to button click. */
-						}}
-						disabled={disabled}
-						className="checkbox self-center"
-					/>
-					{children}
-				</button>
-				<div className="rounded-b-lg py-2 px-8">
-					<div className="py-1">Bitte Begründe deine Antwort:</div>
-					<textarea className="w-full" placeholder="Begründung..."></textarea>
-				</div>
-			</div>
-		);
 	}
 
 	return (
