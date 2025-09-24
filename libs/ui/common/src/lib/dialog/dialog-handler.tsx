6586281d7724794ad36c3a334a875e1220420464
Sascha El-Sharkawy <elscha@sse.uni-hildesheim.de>
2025-09-24 08:45:56 +0000

    doc(common): Documentation of deprecated DocumentHandler
    Documents the alternative to be used instead

--- libs/ui/common/src/lib/dialog/dialog-handler.tsx
+++ libs/ui/common/src/lib/dialog/dialog-handler.tsx
@@ -25,6 +25,12 @@ export const dispatchDialog = (dialog: React.ReactNode, id: string) => {
  * Should be used if the same dialog is opened multiple times in one component
  * Reason React will not rerender the component if the same dialog is used
  * @deprecated
+ * Preferred usage:
+ * ```tsx
+ * const [open, setOpen] = useState(false);
+ * {open && <YourDialog/>}
+ * ```
+ * This may be used to open and close a Dialog at two different, not connected placed, but comes with the drawback of hard maintainable code.
  */
 export const freeDialog = (id: string) => {
 	memoryState = { isOpen: false, dialog: null };
@@ -62,6 +68,12 @@ function useDialog(id: string): State {
 
 /**
  * @deprecated
+ * Preferred usage:
+ * ```tsx
+ * const [open, setOpen] = useState(false);
+ * {open && <YourDialog/>}
+ * ```
+ * This may be used to open and close a Dialog at two different, not connected placed, but comes with the drawback of hard maintainable code.
  */
 export const DialogHandler = memo(DialogHandlerComponent);
 
