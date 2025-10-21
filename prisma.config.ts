78a9a967f4b0c499a345b14aacb631c86b8201aa
Sascha El-Sharkawy <elscha@sse.uni-hildesheim.de>
2025-10-21 06:33:41 +0000

    fix(Prisma): Restored Seed config

--- prisma.config.ts
+++ prisma.config.ts
@@ -4,6 +4,7 @@ import type { PrismaConfig } from "prisma";
 export default {
 	schema: "libs/data-access/database/prisma/schema",
 	migrations: {
-		path: "libs/data-access/database/prisma/migrations"
+		path: "libs/data-access/database/prisma/migrations",
+		seed: "tsx --tsconfig ./libs/data-access/database/tsconfig.lib.json libs/data-access/database/src/lib/seed.ts"
 	}
 } satisfies PrismaConfig;
