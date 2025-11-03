16e10da9a814e6055709b6041d509f5916b5d10c
Sascha El-Sharkawy <elscha@sse.uni-hildesheim.de>
2025-11-03 11:47:41 +0000

    chore: Jest 29.7.0 -> 30.2.0

--- libs/ui/layouts/jest.config.ts
+++ libs/ui/layouts/jest.config.ts
@@ -1,5 +1,7 @@
-/* eslint-disable */
 export default {
+	testEnvironmentOptions: {
+		url: "https://example.com" // -> window.location.origin === "https://example.com"
+	},
 	displayName: "ui-layouts",
 	coverageDirectory: "../../../coverage/libs/ui/layouts",
 	preset: "../../../jest.preset.js"
