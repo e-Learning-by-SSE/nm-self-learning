8bd0b676c9851d67ef345d2e0b41d2906d7c0bb6
archql <artem.drankevich@gmail.com>
2025-11-19 14:51:37 +0000

    fix(date): add missing months and years display

--- libs/util/common/src/lib/date.ts
+++ libs/util/common/src/lib/date.ts
@@ -30,21 +30,31 @@ export function formatDateAgo(date: Date | string | number) {
 export function formatTimeIntervalToString(ms: number): string {
 	const duration = intervalToDuration({ start: 0, end: ms });
 
-	const { days, hours, minutes } = duration;
+	const { years, months, days, hours, minutes } = duration;
 
 	let result = "";
 
+	if (years && years > 0) {
+		result += `${years} ${years > 1 ? "Jahre" : "Jahr"}`;
+	}
+
+	if (months && months > 0) {
+		if (result) result += " ";
+		result += `${months} ${months > 1 ? "Monate" : "Monat"}`;
+	}
+
 	if (days && days > 0) {
+		if (result) result += " ";
 		result += `${days} ${days > 1 ? "Tage" : "Tag"}`;
 	}
 
 	if (hours && hours > 0) {
-		if (result) result += " und ";
+		if (result) result += " ";
 		result += `${hours} ${hours > 1 ? "Stunden" : "Stunde"}`;
 	}
 
 	if (minutes && minutes > 0) {
-		if (result) result += " und ";
+		if (result) result += " ";
 		result += `${minutes} ${minutes > 1 ? "Minuten" : "Minute"}`;
 	}
 	return result || "0 Minuten";
