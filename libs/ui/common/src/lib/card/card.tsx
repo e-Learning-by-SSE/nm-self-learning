d9ad03210a65d70bcba4659bfd6a304d42decf37
ganskem <ganskem@uni-hildesheim.de>
2025-11-20 14:47:01 +0000

    feat(ui): apply new colors to card

--- libs/ui/common/src/lib/card/card.tsx
+++ libs/ui/common/src/lib/card/card.tsx
@@ -13,14 +13,14 @@ export function Card({
 	return (
 		<Link
 			href={href}
-			className="flex flex-col place-items-center gap-4 rounded-lg border border-light-border bg-white pt-4"
+			className="flex flex-col place-items-center gap-4 rounded-lg border border-c-border bg-white pt-4"
 		>
 			<div className="flex aspect-square h-32 w-32 items-center justify-center">
 				{React.cloneElement(imageElement, {
 					className: "h-full w-full object-contain"
 				})}
 			</div>
-			<span className="w-full rounded-b-lg bg-secondary p-4 text-center font-semibold text-white">
+			<span className="w-full rounded-b-lg bg-c-primary p-4 text-center font-semibold text-white">
 				{title}
 			</span>
 		</Link>
