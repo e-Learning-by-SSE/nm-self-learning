3663dd541edd4da3a211160280a7fdff2240d11b
lukas-png <doenges@uni-hildesheim.de>
2025-12-18 14:01:29 +0100

    Revert "feat(thread): added worker pool"
    This reverts commit f7a68ffbfb4cdc2221443aac3b98f442ea8a7341.

--- libs/data-access/api/src/lib/trpc/routers/worker-pool.router.ts
+++
@@ -1,8 +0,0 @@
-import { t } from "../trpc";
-import { workerPoolManager } from "../../workers/worker-pool-manager";
-
-export const workerPoolRouter = t.router({
-	stats: t.procedure.query(() => {
-		return workerPoolManager.getStats();
-	})
-});
