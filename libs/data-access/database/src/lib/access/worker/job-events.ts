7c3565c81f2ebd9e29f9ee49f4a95ad35276dbbb
Sascha El-Sharkawy <elscha@sse.uni-hildesheim.de>
2026-01-29 11:59:09 +0000

    fix(worker-service): Log all job events to DB

--- libs/data-access/database/src/lib/access/worker/job-events.ts
+++ libs/data-access/database/src/lib/access/worker/job-events.ts
@@ -12,48 +12,26 @@ const jobStatusMap: Record<JobEventType, JobStatus> = {
 	finished: JobStatus.FINISHED
 };
 
-const jobRankMap: Record<JobEventType, number> = {
-	queued: 0,
-	started: 1,
-	aborted: 2,
-	finished: 2
-};
-
 export async function logJobProgress(jobId: string, event: JobEvent) {
 	if (event.type !== "ready") {
 		let cause = event.type === "aborted" ? event.cause : undefined;
 		// !!! Only for testing, will be removed later, do not use for production !!!
 		cause = event.type === "finished" && event.result ? JSON.stringify(event.result) : cause;
 
-		return database.$transaction(async tx => {
-			const existing = await tx.jobQueue.findUnique({
-				where: { id: jobId },
-				select: { status: true }
-			});
-
-			let shouldUpdate = !existing;
-			if (existing) {
-				const existingRank = jobRankMap[existing.status as JobEventType];
-				shouldUpdate = jobRankMap[event.type] >= existingRank;
-			}
-
-			// Only insert/update if new event status is more advanced than existing status
-			if (shouldUpdate) {
-				return tx.jobQueue.upsert({
-					where: { id: jobId },
-					create: {
-						id: jobId,
-						jobType: "HelloWorld",
-						status: jobStatusMap[event.type],
-						cause
-					},
-					update: {
-						status: jobStatusMap[event.type],
-						cause
-					}
-				});
-			} else {
-				return undefined;
+		// Always upsert without checking existing status
+		// Better would be to check if state is newer than before, but this requires transactions,
+		// which may in turn block events of being saved
+		return database.jobQueue.upsert({
+			where: { id: jobId },
+			create: {
+				id: jobId,
+				jobType: "HelloWorld",
+				status: jobStatusMap[event.type],
+				cause
+			},
+			update: {
+				status: jobStatusMap[event.type],
+				cause
 			}
 		});
 	} else {
