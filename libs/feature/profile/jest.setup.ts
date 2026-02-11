50a5a91597fde6b8af890b7381faa61a8418a2f8
Sascha El-Sharkawy <elscha@sse.uni-hildesheim.de>
2026-02-11 08:55:10 +0000

    Avoid react warning

--- libs/feature/profile/jest.setup.ts
+++ libs/feature/profile/jest.setup.ts
@@ -2,3 +2,15 @@ import { TextEncoder, TextDecoder } from "web-encoding";
 
 global.TextEncoder = TextEncoder;
 global.TextDecoder = TextDecoder;
+
+// Avoid warning:
+// react-i18next:: useTranslation: You will need to pass in an i18next instance by using initReactI18next { code: 'NO_I18NEXT_INSTANCE' }
+jest.mock("react-i18next", () => ({
+	useTranslation: () => ({
+		t: (key: string) => key,
+		i18n: { language: "en", changeLanguage: async () => {} }
+	}),
+	Trans: ({ i18nKey }: any) => i18nKey,
+	I18nextProvider: ({ children }: any) => children,
+	initReactI18next: { type: "3rdParty", init: () => {} }
+}));
