fe5b927a5faa8691fe446fdd8fe56b71dc6af27b
Sascha El-Sharkawy <elscha@sse.uni-hildesheim.de>
2026-04-07 07:07:43 +0000

    feat(subtitle): Improved VTT processing
    -Trimmed strings
    -date-fs for formatting

--- libs/util/common/src/lib/video-subtitle/subtitle-converter.ts
+++ libs/util/common/src/lib/video-subtitle/subtitle-converter.ts
@@ -1,23 +1,17 @@
 import { SubtitleSrc } from "@self-learning/types";
+import { format } from "date-fns";
 
 export async function ConvertTranscriptionToSubtitle(transcription: SubtitleSrc): Promise<string> {
-    let vttString = "WEBVTT\n\n";
-    transcription.segments.forEach((segment) => {
-        vttString += `${convertSecondsToVttTime(segment.start)} --> ${convertSecondsToVttTime(segment.end)} \n ${segment.text}\n\n`;
-    });
-    return vttString;
+	let vttString = "WEBVTT\n\n";
+	transcription.segments.forEach(segment => {
+		vttString += `${convertSecondsToVttTime(segment.start)} --> ${convertSecondsToVttTime(segment.end)} \n ${segment.text.trim()}\n\n`;
+	});
+	return vttString;
 }
 
+// Converts seconds to VTT time format (HH:MM:SS.mmm)
+// Supports only up to 24 hours, which is sufficient for video subtitles
 function convertSecondsToVttTime(seconds: number): string {
-    const hours = Math.floor(seconds / 3600);
-    const minutes = Math.floor((seconds % 3600) / 60);
-    const remainingSeconds = Math.floor(seconds % 60);
-    const milliseconds = Math.floor((seconds - Math.floor(seconds)) * 1000);
-
-    const twoDigitHourse = hours.toString().padStart(2, "0");
-    const twoDigitMinutes = minutes.toString().padStart(2, "0");
-    const twoDigitSeconds = remainingSeconds.toString().padStart(2, "0");
-    const threeDigitMilliseconds = milliseconds.toString().padStart(3, "0");
-
-    return `${twoDigitHourse}:${twoDigitMinutes}:${twoDigitSeconds}.${threeDigitMilliseconds}`;
+	const date = new Date(seconds * 1000);
+	return format(date, "HH:mm:ss.SSS");
 }
