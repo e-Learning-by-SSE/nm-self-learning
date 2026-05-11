1eafc78bae19c9971638a3b217bec49be4aaeca0
archql <artem.drankevich@gmail.com>
2026-05-11 09:57:24 +0000

    fix(eslint): plugin duplicate error

--- apps/site/eslint.config.mjs
+++ apps/site/eslint.config.mjs
@@ -1,21 +1,11 @@
-import { FlatCompat } from "@eslint/eslintrc";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import js from "@eslint/js";
 import baseConfig from "../../eslint.config.mjs";
 import globals from "globals";
-
-const compat = new FlatCompat({
-	baseDirectory: dirname(fileURLToPath(import.meta.url)),
-	recommendedConfig: js.configs.recommended
-});
 
 export default [
 	{
 		ignores: ["**/dist"]
 	},
 	...baseConfig,
-	...compat.extends("plugin:@nx/react-typescript", "next", "next/core-web-vitals"),
 	{ languageOptions: { globals: { ...globals.jest } } },
 	{
 		files: ["**/*.ts", "**/*.tsx", "**/*.js", "**/*.jsx"],
@@ -25,12 +15,10 @@ export default [
 	},
 	{
 		files: ["**/*.ts", "**/*.tsx"],
-		// Override or add rules here
 		rules: {}
 	},
 	{
 		files: ["**/*.js", "**/*.jsx"],
-		// Override or add rules here
 		rules: {}
 	}
 ];
--- libs/feature/ai-tutor/eslint.config.mjs
+++ libs/feature/ai-tutor/eslint.config.mjs
@@ -1,20 +1,10 @@
-import { FlatCompat } from "@eslint/eslintrc";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import js from "@eslint/js";
 import baseConfig from "../../../eslint.config.mjs";
-
-const compat = new FlatCompat({
-	baseDirectory: dirname(fileURLToPath(import.meta.url)),
-	recommendedConfig: js.configs.recommended
-});
 
 export default [
 	{
 		ignores: ["**/dist"]
 	},
 	...baseConfig,
-	...compat.extends("plugin:@nx/react"),
 	{
 		files: ["**/*.ts", "**/*.tsx", "**/*.js", "**/*.jsx"],
 		rules: {}
