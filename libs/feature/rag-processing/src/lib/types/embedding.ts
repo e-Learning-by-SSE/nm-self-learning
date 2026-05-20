1a27b3e63ae717fb38e77d28d46c9145504fb67f
Sascha El-Sharkawy <elscha@sse.uni-hildesheim.de>
2026-05-20 02:55:11 +0000

    rf(rag): Removal of dynamic imports
    Dynamic imports complicate the development of multiple projects which different bundler strategies (site, cron-service, worker-service). For this reason the following dynamic imports were removed
    - vectore-store: await import("./embedding.js");
    - content-processor:  await import("pdf-parse")
    
    In addition: IEmbeddingService was only used internally. Since TS uses type inference, there is no need for the definition of an interface (that's a Jva paradigm). For ths reason, also this interface is removed.
    
    Closes #509

--- libs/feature/rag-processing/src/lib/types/embedding.ts
+++
@@ -1,6 +0,0 @@
-export interface IEmbeddingService {
-	initialize(): Promise<void>;
-	generateEmbedding(text: string): Promise<number[]>;
-	generateBatchEmbeddings(texts: string[]): Promise<number[][]>;
-	isInitialized(): boolean;
-}
