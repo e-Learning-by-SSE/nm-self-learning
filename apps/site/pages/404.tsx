14e2bc0b21391bb1d64564fc7890bfe29898e949
Sascha El-Sharkawy <elscha@sse.uni-hildesheim.de>
2026-06-09 05:28:57 +0000

    fix(site): Hydration error on 404
    404 doesn't support ServerSideProps, but static props
    - https://nextjs.org/docs/messages/404-get-initial-props
    - https://nextjs.org/docs/pages/building-your-application/routing/custom-error#404-page

--- apps/site/pages/404.tsx
+++ apps/site/pages/404.tsx
@@ -1,6 +1,13 @@
 import { withTranslations } from "@self-learning/api";
 
-export const getServerSideProps = withTranslations(["common"]);
+import type { GetStaticProps } from "next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+
+export const getStaticProps: GetStaticProps = async ({ locale }) => ({
+	props: {
+		...(await serverSideTranslations(locale ?? "de", ["common"]))
+	}
+});
 
 export default function PageNotFound() {
 	return <div>404</div>;
